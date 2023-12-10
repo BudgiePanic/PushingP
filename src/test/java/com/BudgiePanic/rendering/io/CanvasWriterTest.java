400fec82de887ccad589b94f4d0097ce18cb108e
BudgiePanic <bsanson90@gmail.com>
2023-12-11 11:13:54 +1300

    test: test pixel write

--- src/test/java/com/BudgiePanic/rendering/io/CanvasWriterTest.java
+++ src/test/java/com/BudgiePanic/rendering/io/CanvasWriterTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import com.BudgiePanic.rendering.util.ArrayCanvas;
+import com.BudgiePanic.rendering.util.Color;
 
 /**
  * Canvas File IO tests. PPM format.
@@ -22,4 +23,15 @@ public class CanvasWriterTest {
         assertEquals("255", outputString.get(2)); // color range: [0 - 255] 256 possible values
     }
 
+    @Test 
+    void testPPMPixelWrite() {
+        var canvas = new ArrayCanvas(5, 3);
+        canvas.writePixel(0, 0, new Color(1.5f, 0f, 0f));
+        canvas.writePixel(2, 1, new Color(0f, 0.5f, 0f));
+        canvas.writePixel(4, 2, new Color(-0.5f, 0f, 1f));
+        var outputString = CanvasWriter.canvasToPPMString(canvas);
+        assertEquals("255 0 0 0 0 0 0 0 0 0 0 0 0 0 0", outputString.get(3));
+        assertEquals("0 0 0 0 0 0 0 128 0 0 0 0 0 0 0", outputString.get(4));
+        assertEquals("0 0 0 0 0 0 0 0 0 0 0 0 0 0 255", outputString.get(5));
+    }
 }
