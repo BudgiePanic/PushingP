a582f83f7bbccbe96a268847730bb6f04cd4710e
BudgiePanic <bsanson90@gmail.com>
2023-12-11 13:32:38 +1300

    misc: tweak artillery toy parameters

--- src/main/java/com/BudgiePanic/rendering/App.java
+++ src/main/java/com/BudgiePanic/rendering/App.java
@@ -15,10 +15,11 @@ public class App
     {
         System.out.println( "Hello World!" );
         if (args.length > 0 && args[0].equals(flagArtillery)) {
+            System.out.println("Running artillery toy.");
             new Artillery(
-                Tuple.makePoint(0f, 0.1f, 0f),
-                Tuple.makeVector(1f, 1f, 0f),
-                Tuple.makeVector(0f, -9.8f, 0f),
+                Tuple.makePoint(0f, 1f, 0f),
+                Tuple.makeVector((0.1f) * 100f,  (0.1f) * 100f, 0f),
+                Tuple.makeVector(0f, (0.1f) * -9.8f, 0f),
                 Tuple.makeVector(1f, 0f, 0f))
                 .run();
         }
