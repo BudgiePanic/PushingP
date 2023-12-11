9be39f7397f2bbc7732621e84b7f2ad8125a3337
BudgiePanic <bsanson90@gmail.com>
2023-12-11 13:14:22 +1300

    feat: implement artillery toy simulation

--- src/main/java/com/BudgiePanic/rendering/toy/Artillery.java
+++ src/main/java/com/BudgiePanic/rendering/toy/Artillery.java
@@ -1,6 +1,16 @@
 package com.BudgiePanic.rendering.toy;
 
+import com.BudgiePanic.rendering.io.CanvasWriter;
+import com.BudgiePanic.rendering.util.ArrayCanvas;
+import com.BudgiePanic.rendering.util.Canvas;
+import com.BudgiePanic.rendering.util.Color;
+import com.BudgiePanic.rendering.util.Colors;
 import com.BudgiePanic.rendering.util.Tuple;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
 
 /**
  * Runs a simple projectile simulation.
@@ -15,12 +25,17 @@ public final class Artillery implements Runnable{
     private Tuple vel;
     private Tuple gravity;
     private Tuple wind;
+    private Canvas canvas;
+    private static final int maxIterations = 2000000;
+    private static final String fileName = "image.ppm";
 
     public Artillery(Tuple projectilePosition, Tuple projectileVelocity, Tuple gravity, Tuple wind) {
         this.proj = projectilePosition;
         this.vel = projectileVelocity;
         this.gravity = gravity;
         this.wind = wind;
+        if (gravity.y <= 0f) gravity = new Tuple(gravity.x, -9.8f, gravity.z, gravity.w);
+        this.canvas = new ArrayCanvas(500, 500);
     }
 
     /**
@@ -36,11 +51,33 @@ public final class Artillery implements Runnable{
 
     @Override
     public void run() {
-        while (proj.y > 0f) {
+        final Color projColor = Colors.white;
+        final int canvasHeight = canvas.getHeight();
+        final int canvasWidth = canvas.getWidth();
+        int iterations = 0;
+        while (proj.y > 0f && iterations < maxIterations) {
+            // Write the position of the projectile to the canvas
+            int projX = (int) proj.x;
+            int projY = canvasHeight - (int) proj.y;
+            if (
+                projX >= 0 &&
+                projX < canvasWidth &&
+                projY >= 0 &&
+                projY < canvasHeight
+            ) {
+                canvas.writePixel(projX, projY, projColor);
+            }
+            // run the simulation by one tick
             tick(0.1f);
+            iterations++;
+        }
+        // write the canvas to disc for viewing
+        var lines = CanvasWriter.canvasToPPMString(canvas);
+        File file = new File(System.getProperty("user.dir"), fileName);
+        try {
+            FileUtils.writeLines(file, lines);
+        } catch (IOException e) {
+            System.err.println(e);
         }
     }  
-
-
-
 }
