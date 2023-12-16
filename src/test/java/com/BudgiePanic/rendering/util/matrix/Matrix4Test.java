ecdf35ab347a0a7c27a2a31c5cd729e4eb8981de
BudgiePanic <bsanson90@gmail.com>
2023-12-16 16:36:51 +1300

    fix: correct test intput

--- src/test/java/com/BudgiePanic/rendering/util/matrix/Matrix4Test.java
+++ src/test/java/com/BudgiePanic/rendering/util/matrix/Matrix4Test.java
@@ -268,7 +268,7 @@ public class Matrix4Test {
     @Test
     void testMat4Inverse() {
         var mat = Matrix4.buildMatrix(
-            -5, 2, 6, 8,  
+            -5, 2, 6, -8,  
             1, -5, 1, 8, 
             7, 7, -6, -7,
             1, -3, 7, 4
