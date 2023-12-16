4922eca64050d5e7d388a5f59e10f673debf43b7
BudgiePanic <bsanson90@gmail.com>
2023-12-16 13:45:56 +1300

    feat: implement mat2 determinant operation

--- src/main/java/com/BudgiePanic/rendering/util/matrix/Matrix2.java
+++ src/main/java/com/BudgiePanic/rendering/util/matrix/Matrix2.java
@@ -119,4 +119,9 @@ public final class Matrix2 extends Matrix {
         return true;
     }
 
+    @Override
+    public float getDeterminant() {
+        return (matrix[0][0] * matrix[1][1]) - (matrix[0][1] * matrix[1][0]);
+    }
+
 }
