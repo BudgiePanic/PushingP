e8a402d6686952f3a6b415c71833f231ba31dcfd
BudgiePanic <bsanson90@gmail.com>
2023-12-18 17:54:14 +1300

    feat: implement ray transform method

--- src/main/java/com/BudgiePanic/rendering/util/intersect/Ray.java
+++ src/main/java/com/BudgiePanic/rendering/util/intersect/Ray.java
@@ -1,6 +1,7 @@
 package com.BudgiePanic.rendering.util.intersect;
 
 import com.BudgiePanic.rendering.util.Tuple;
+import com.BudgiePanic.rendering.util.matrix.Matrix4;
 
 /**
  * Ray type.
@@ -25,5 +26,17 @@ public record Ray (Tuple origin, Tuple direction) {
      */
     public Tuple position(float delta) {
         return this.origin.add(this.direction.multiply(delta));
+    }
+
+    /**
+     * Create a new ray, transformed by the matrix.
+     *
+     * @param matrix
+     *   The new transform matrix.
+     * @return
+     *   A new ray after it was passed through the matrix transform
+     */
+    public Ray transform(Matrix4 matrix) {
+        return new Ray(matrix.multiply(this.origin), matrix.multiply(this.direction()));
     }
 }
