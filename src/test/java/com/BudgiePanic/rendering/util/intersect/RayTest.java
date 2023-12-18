3654d9fdd1d3d99250e19f0d4013f477f80430c0
BudgiePanic <bsanson90@gmail.com>
2023-12-18 17:53:04 +1300

    test: add ray transform tests

--- src/test/java/com/BudgiePanic/rendering/util/intersect/RayTest.java
+++ src/test/java/com/BudgiePanic/rendering/util/intersect/RayTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 import com.BudgiePanic.rendering.util.Tuple;
+import com.BudgiePanic.rendering.util.transform.Transforms;
 
 /**
  * Unit tests for the ray class.
@@ -33,6 +34,25 @@ public class RayTest {
         assertEquals(Tuple.makePoint(3, 3, 4), ray.position(1f));
         assertEquals(Tuple.makePoint(1, 3, 4), ray.position(-1f));
         assertEquals(Tuple.makePoint(4.5f, 3, 4), ray.position(2.5f));
+    }
+
+    @Test
+    void testRayTransform() {
+        var ray = new Ray(Tuple.makePoint(1, 2, 3), Tuple.makeVector(0, 1, 0));
+        var transform = Transforms.identity().translate(3, 4, 5).assemble();
+        // The book wants us to test for immutability here, but Tuples and Rays are already immutable by design :)
+        var result = ray.transform(transform);
+        var expected = new Ray(Tuple.makePoint(4, 6, 8), Tuple.makeVector(0, 1, 0));
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void testRayTransformA() {
+        var ray = new Ray(Tuple.makePoint(1, 2, 3), Tuple.makeVector(0, 1, 0));
+        var transform = Transforms.identity().scale(2, 3, 4).assemble();
+        var result = ray.transform(transform);
+        var expected = new Ray(Tuple.makePoint(2, 6, 12), Tuple.makeVector(0, 3, 0));
+        assertEquals(expected, result);
     }
 
 }
