f11e4607486970f222defb4871361053fa89abfb
BudgiePanic <bsanson90@gmail.com>
2024-01-23 13:50:42 +1300

    test: add test for intersection within shape
    intersections that originate from within
    a shape should have their normal vector
    flipped and should set the flag in the
    shading container

--- src/test/java/com/BudgiePanic/rendering/util/intersect/ShadingInfoTest.java
+++ src/test/java/com/BudgiePanic/rendering/util/intersect/ShadingInfoTest.java
@@ -1,6 +1,8 @@
 package com.BudgiePanic.rendering.util.intersect;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -19,6 +21,27 @@ public class ShadingInfoTest {
         assertEquals(intersection.a(), result.a());
         assertEquals(intersection.sphere(), result.shape());
         assertEquals(Tuple.makePoint(0, 0, -1), result.point());
+        assertEquals(Tuple.makeVector(0, 0, -1), result.eyeVector());
+        assertEquals(Tuple.makeVector(0, 0, -1), result.normalVector());
+    }
+
+    @Test
+    void testIntersectionOutsideShape() {
+        var ray = new Ray(Tuple.makePoint(0, 0, -5), Tuple.makeVector(0, 0, 1));
+        var shape = Sphere.defaultSphere();
+        var intersection = new Intersection(4f, shape);
+        var result = intersection.computeShadingInfo(ray);
+        assertFalse(result.intersectInside());
+    }
+
+    @Test
+    void testIntersectionInsideShape() {
+        var ray = new Ray(Tuple.makePoint(), Tuple.makeVector(0, 0, 1));
+        var shape = Sphere.defaultSphere();
+        var intersection = new Intersection(1f, shape);
+        var result = intersection.computeShadingInfo(ray);
+        assertTrue(result.intersectInside());
+        assertEquals(Tuple.makePoint(0, 0, 1), result.point());
         assertEquals(Tuple.makeVector(0, 0, -1), result.eyeVector());
         assertEquals(Tuple.makeVector(0, 0, -1), result.normalVector());
     }
