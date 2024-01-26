5bc9fe8d48f996dcf0d0a22ece7267f129bb9533
BudgiePanic <bsanson90@gmail.com>
2024-01-26 23:36:50 +1300

    feat: add method to get point above surface

--- src/main/java/com/BudgiePanic/rendering/util/intersect/ShadingInfo.java
+++ src/main/java/com/BudgiePanic/rendering/util/intersect/ShadingInfo.java
@@ -1,5 +1,6 @@
 package com.BudgiePanic.rendering.util.intersect;
 
+import com.BudgiePanic.rendering.util.FloatHelp;
 import com.BudgiePanic.rendering.util.Tuple;
 import com.BudgiePanic.rendering.util.shape.Sphere;
 
@@ -8,4 +9,15 @@ import com.BudgiePanic.rendering.util.shape.Sphere;
  * 
  * @author BudgiePanic
  */
-public record ShadingInfo(float a, Sphere shape, Tuple point, Tuple eyeVector, Tuple normalVector, boolean intersectInside) {}
+public record ShadingInfo(float a, Sphere shape, Tuple point, Tuple eyeVector, Tuple normalVector, boolean intersectInside) {
+
+    /**
+     * Calculate a point slightly above the surface that is being shaded, to avoid floating point precision errors.
+     *
+     * @return
+     *   A point above the surface slightly along the normal direction.
+     */
+    public Tuple overPoint() {
+        return point.add(normalVector.multiply(3f*FloatHelp.epsilon));
+    }
+}
