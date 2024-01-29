3aeb85acd0c56a53ce02001bc2873a87dce76460
BudgiePanic <bsanson90@gmail.com>
2024-01-29 16:00:54 +1300

    test: add tests for shared shape functionality

---
+++ src/test/java/com/BudgiePanic/rendering/util/shape/BaseShapeTest.java
@@ -0,0 +1,83 @@
+package com.BudgiePanic.rendering.util.shape;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+
+import com.BudgiePanic.rendering.util.Tuple;
+import com.BudgiePanic.rendering.util.intersect.Intersection;
+import com.BudgiePanic.rendering.util.intersect.Ray;
+import com.BudgiePanic.rendering.util.matrix.Matrix4;
+import com.BudgiePanic.rendering.util.transform.Transforms;
+
+public class BaseShapeTest {
+    
+    private static class TestShape extends BaseShape {
+
+        public Ray localIntersectRayResult;
+
+        public TestShape(Matrix4 transform) {
+            super(transform);
+        }
+
+        @Override
+        protected Optional<List<Intersection>> localIntersect(Ray ray) {
+            // This will allow us to check that BaseShape is correctly transforming the ray to object space
+            // before calling local intersect
+            this.localIntersectRayResult = ray;
+            return Optional.empty();
+        }
+
+        @Override
+        protected Tuple localNormal(Tuple point) {
+            // this will allow us to check that BaseShape is correctly transforming the point before
+            // passing it to localNormal
+            return Tuple.makeVector(point.x, point.y, point.z); 
+        }
+
+    }
+
+    @Test
+    void testScaledShapeRayIntersection() {
+        // Replaces SphereTest::testSphereRayTransform
+        var ray = new Ray(Tuple.makePoint(0, 0, -5), Tuple.makeVector(0, 0, 1));
+        var shape = new TestShape(Transforms.identity().scale(2, 2, 2).assemble());
+        @SuppressWarnings(value = { "unused" }) 
+        var intersections = shape.intersect(ray); // force the ray to pass through BaseShape's methods.
+        assertEquals(Tuple.makePoint(0, 0, -2.5f), shape.localIntersectRayResult.origin());
+        assertEquals(Tuple.makeVector(0, 0, 0.5f), shape.localIntersectRayResult.direction());
+    }
+
+    @Test
+    void testTranslatedShapeRayIntersection() {
+        // Replaces SphereTest::testSphereRayTransformA
+        var ray = new Ray(Tuple.makePoint(0, 0, -5), Tuple.makeVector(0, 0, 1));
+        var shape = new TestShape(Transforms.identity().translate(5, 0, 0).assemble());
+        @SuppressWarnings(value = { "unused" }) 
+        var intersections = shape.intersect(ray); // force the ray to pass through BaseShape's methods.
+        assertEquals(Tuple.makePoint(-5, 0, -5), shape.localIntersectRayResult.origin());
+        assertEquals(Tuple.makeVector(0, 0, 1), shape.localIntersectRayResult.direction());
+    }
+
+    @Test
+    void testTranslatedShapeNormalTransform() {
+        // Replaces SphereTest::testNormalNonOrigin
+        var shape = new TestShape(Transforms.identity().translate(0, 1, 0).assemble());
+        var result = shape.normal(Tuple.makePoint(0, 1.70711f, -0.70711f));
+        assertEquals(Tuple.makeVector(0, 0.70711f, -0.70711f), result);
+    }
+
+    @Test
+    void testTransformedShapeNormal() {
+        // Replaces SphereTest::testNormalTransformedOrigin
+        final float piOverFive = (float) (Math.PI / 5.0);
+        final float sqrtTwoOverTwo = (float) (Math.sqrt(2.0) / 2.0);
+        var shape = new TestShape(Transforms.identity().rotateZ(piOverFive).scale(1, 0.5f, 1).assemble());
+        var result = shape.normal(Tuple.makePoint(0, sqrtTwoOverTwo, -sqrtTwoOverTwo));
+        assertEquals(Tuple.makeVector(0, 0.97014f, -0.25254f), result);
+    }
+
+}
