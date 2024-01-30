e63ab4fd88587df0d21b48b06fa44f8ddb5f307e
BudgiePanic <bsanson90@gmail.com>
2024-01-30 14:55:51 +1300

    test: add stripe transform tests

--- src/test/java/com/BudgiePanic/rendering/util/pattern/StripeTest.java
+++ src/test/java/com/BudgiePanic/rendering/util/pattern/StripeTest.java
@@ -6,6 +6,9 @@ import static com.BudgiePanic.rendering.util.Tuple.makePoint;
 import org.junit.jupiter.api.Test;
 
 import com.BudgiePanic.rendering.util.Colors;
+import com.BudgiePanic.rendering.util.Material;
+import com.BudgiePanic.rendering.util.shape.Sphere;
+import com.BudgiePanic.rendering.util.transform.Transforms;
 
 public class StripeTest {
     
@@ -34,5 +37,41 @@ public class StripeTest {
         assertEquals(Colors.black, stripe.colorAt(makePoint(1f, 0f, 0f)));
         assertEquals(Colors.black, stripe.colorAt(makePoint(-0.1f, 0f, 0f)));
         assertEquals(Colors.black, stripe.colorAt(makePoint(-1f, 0f, 0f)));
+    }
+
+    @Test
+    void testStripeWithShapeTransform() {
+        var shape = new Sphere(Transforms.identity().scale(2, 2, 2).assemble(), Material.pattern(new Stripe(Colors.white, Colors.black)));
+        var output = shape.material().pattern().get().colorAt(makePoint(1.5f, 0, 0), shape);
+        assertEquals(Colors.white, output);
+    }
+
+    @Test 
+    void testStripeWithLocalTransform() {
+        var shape = new Sphere(
+            Transforms.identity().assemble(), 
+            Material.pattern(new Stripe(
+                Colors.white, 
+                Colors.black,
+                Transforms.identity().scale(2, 2, 2).assemble()
+                )
+            )
+        );
+        var output = shape.material().pattern().get().colorAt(makePoint(1.5f, 0, 0), shape);
+        assertEquals(Colors.white, output);
+    }
+
+    @Test
+    void testStripeWithShapeLocalTransform() {
+        var shape = new Sphere(
+            Transforms.identity().scale(2, 2, 2).assemble(),
+            Material.pattern(
+                new Stripe(Colors.white, Colors.black, 
+                Transforms.identity().translate(0.5f, 0, 0).assemble()
+                )
+            )
+        );
+        var output = shape.material().pattern().get().colorAt(makePoint(2.5f, 0, 0), shape);
+        assertEquals(Colors.white, output);
     }
 }
