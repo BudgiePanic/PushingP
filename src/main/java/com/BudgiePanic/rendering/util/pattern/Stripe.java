79dd2de1dba0746ff86fbc8669619a03dec3bcb6
BudgiePanic <bsanson90@gmail.com>
2024-01-30 14:56:33 +1300

    refactor: create default stripe constructor
    the default constructor auto populates with an identity transform

--- src/main/java/com/BudgiePanic/rendering/util/pattern/Stripe.java
+++ src/main/java/com/BudgiePanic/rendering/util/pattern/Stripe.java
@@ -2,6 +2,7 @@ package com.BudgiePanic.rendering.util.pattern;
 
 import com.BudgiePanic.rendering.util.Color;
 import com.BudgiePanic.rendering.util.Tuple;
+import com.BudgiePanic.rendering.util.matrix.Matrix4;
 
 /**
  * The stripe pattern alternates between two colors as the x coordinate moves along the pattern.
@@ -13,10 +14,14 @@ import com.BudgiePanic.rendering.util.Tuple;
  * 
  * @author BudgiePanic
  */
-public record Stripe(Color a, Color b) implements Pattern {
+public record Stripe(Color a, Color b, Matrix4 transform) implements Pattern {
+
+    public Stripe(Color a, Color b) {
+        this(a, b, Matrix4.identity());
+    }
 
     @Override
-    public Color colorAt(Tuple point) {
+    public final Color colorAt(Tuple point) {
         if (((int)Math.floor(point.x)) % 2 == 0) {
             return a();
         } else {
