e2df397f87a4dd08be5f0be4f66b2869b5a9b40e
BudgiePanic <bsanson90@gmail.com>
2024-02-07 23:39:05 +1300

    feat: implement pattern demo

---
+++ src/main/java/com/BudgiePanic/rendering/toy/PatternToy.java
@@ -0,0 +1,105 @@
+package com.BudgiePanic.rendering.toy;
+
+import static com.BudgiePanic.rendering.util.Tuple.makePoint;
+import static com.BudgiePanic.rendering.util.pattern.BiOperation.blend;
+import static com.BudgiePanic.rendering.util.pattern.BiOperation.radialGradient;
+import static com.BudgiePanic.rendering.util.pattern.BiOperation.ring;
+import static com.BudgiePanic.rendering.util.pattern.BiOperation.stripe;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+
+import com.BudgiePanic.rendering.io.CanvasWriter;
+import com.BudgiePanic.rendering.scene.Camera;
+import com.BudgiePanic.rendering.scene.World;
+import com.BudgiePanic.rendering.util.AngleHelp;
+import com.BudgiePanic.rendering.util.Canvas;
+import com.BudgiePanic.rendering.util.Color;
+import com.BudgiePanic.rendering.util.Colors;
+import com.BudgiePanic.rendering.util.Material;
+import com.BudgiePanic.rendering.util.Tuple;
+import com.BudgiePanic.rendering.util.light.PointLight;
+import com.BudgiePanic.rendering.util.pattern.BiOperation;
+import com.BudgiePanic.rendering.util.pattern.BiPattern;
+import com.BudgiePanic.rendering.util.pattern.Perturb;
+import com.BudgiePanic.rendering.util.pattern.SolidColor;
+import com.BudgiePanic.rendering.util.shape.Plane;
+import com.BudgiePanic.rendering.util.shape.Shape;
+import com.BudgiePanic.rendering.util.shape.Sphere;
+import com.BudgiePanic.rendering.util.transform.Transforms;
+import com.BudgiePanic.rendering.util.transform.View;
+
+public class PatternToy implements Runnable {
+
+    private final static String fileName = "pattern_demo.ppm";
+    private final static int width = 700, height = 700;
+    private final static float fov = AngleHelp.toRadians(65f);
+    private final Camera camera = new Camera(width, height, fov, View.makeViewMatrix(Tuple.makePoint(0,5,-4.5f), Tuple.makePoint(0, 1, 1), Tuple.makePoint(0,1,0)));
+
+    @Override
+    public void run() {
+        System.out.println("Building world");
+        final List<Shape> shapes = List.of(
+            new Plane(Transforms.identity().assemble(), 
+            Material.pattern(
+                new Perturb(
+                    new BiPattern(BiOperation.radialGradient, 
+                        new BiPattern(BiOperation.checker, new SolidColor(Colors.green), new SolidColor(Colors.blue)),
+                        new BiPattern(BiOperation.ring, new SolidColor(Colors.white), new SolidColor(Colors.red)))
+                    ))),
+            
+            new Sphere(
+                Transforms.identity().translate(-4, 2f, 1).assemble(), 
+                Material.pattern(
+                    new Perturb(new Perturb(new Perturb(
+                        new BiPattern(BiOperation.ring, Colors.blue, Colors.white, Transforms.identity().scale(0.25f, 0.25f, 0.25f).assemble()), 0.1f),
+                        0.25f)
+                    ))),
+
+            new Sphere(
+                Transforms.identity().translate(4, 2f, 1).assemble(),
+                Material.pattern(new BiPattern(blend, 
+                        new BiPattern(stripe, new Color(0.33f, 0.33f, 0), new Color(0.5f,0.5f,0.5f)),
+                        new BiPattern(stripe, Colors.black, new Color(0.5f, 0, 0.33f), 
+                            Transforms.identity().rotateX(AngleHelp.toRadians(90f)).assemble()),
+                    Transforms.identity().scale(0.33f, 0.33f, 0.33f).assemble()))),
+
+            new Sphere(Transforms.identity().translate(0, 3, 1).assemble(),
+                Material.pattern(new BiPattern(blend,
+                    new Perturb(new BiPattern(ring, new Color(0.3f, 0.85f, 0.8f), new Color(0.31f, 0.9f, 0.45f), 
+                        Transforms.identity().scale(0.2f, 0.2f, 0.2f).assemble())),
+                    new BiPattern(radialGradient, new Color(0.93f, 0.25f, 0.13f), new Color(0.95f, 0.13f, 0.608f), 
+                    Transforms.identity().scale(0.33f, 0.33f, 0.33f).assemble()),
+                    Transforms.identity().scale(0.45f, 0.45f, 0.45f).assemble())))
+        );
+
+        new Perturb(new BiPattern(BiOperation.radialGradient, 
+                        new BiPattern(BiOperation.checker, new SolidColor(Colors.green), new SolidColor(Colors.blue)),
+                        new BiPattern(BiOperation.ring, new SolidColor(Colors.white), new SolidColor(Colors.red))));
+
+        final List<PointLight> lights = List.of(
+            new PointLight(makePoint(3, 5, -2), Colors.white)
+        );
+
+        World world = new World();
+        world.getShapes().addAll(shapes);
+        world.getLights().addAll(lights);
+
+        System.out.println("taking image");
+        Canvas canvas = camera.takePicture(world);
+
+        System.out.println("saving image");
+        var lines = CanvasWriter.canvasToPPMString(canvas);
+        File file = new File(System.getProperty("user.dir"), fileName);
+        try {
+            FileUtils.writeLines(file, lines);
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+        System.out.println("done");
+    }
+    
+}
