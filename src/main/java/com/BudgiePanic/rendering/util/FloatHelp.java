4f234ea54776549e9f5790d197d0d44c2aca2b9b
BudgiePanic <bsanson90@gmail.com>
2024-02-17 17:36:18 +1300

    refactor: update epsilon value
    on the ray tracer challenge wiki, the book author advises that the value 0.0001 should be used and that 0.00001 is too much

--- src/main/java/com/BudgiePanic/rendering/util/FloatHelp.java
+++ src/main/java/com/BudgiePanic/rendering/util/FloatHelp.java
@@ -12,13 +12,7 @@ public final class FloatHelp {
     /**
      * The maximum difference in floats for them to be considered the same.
      */
-    public final static float epsilon = 0.00001f;
-
-    /**
-     * A quantity that has been observed to be larger that the accumulated floating point errors in some calculations. (largest error seen so far = 0.000390)
-     * Used to check if a calculation result is close to zero when it has accumulated many floating point errors.
-     */
-    public final static float bigEpsilon = 0.001f;
+    public final static float epsilon = 0.0001f;
     
     /**
      * checks if two floating point numbers are similar enough to each other.
