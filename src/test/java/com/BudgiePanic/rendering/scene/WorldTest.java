3866c778674d3198cd872c54a0d851cf9d357d4c
BudgiePanic <bsanson90@gmail.com>
2024-02-18 22:34:02 +1300

    misc: remove unused import

--- src/test/java/com/BudgiePanic/rendering/scene/WorldTest.java
+++ src/test/java/com/BudgiePanic/rendering/scene/WorldTest.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.List;
 import java.util.Optional;
