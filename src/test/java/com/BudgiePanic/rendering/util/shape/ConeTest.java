79c9788c5f0b1a2d52f497ba29c52081c3683e73
BudgiePanic <bsanson90@gmail.com>
2024-02-20 11:15:56 +1300

    misc: remove unused import

--- src/test/java/com/BudgiePanic/rendering/util/shape/ConeTest.java
+++ src/test/java/com/BudgiePanic/rendering/util/shape/ConeTest.java
@@ -4,7 +4,6 @@ import static com.BudgiePanic.rendering.util.Tuple.makePoint;
 import static com.BudgiePanic.rendering.util.Tuple.makeVector;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.List;
 
