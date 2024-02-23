ed014bec05fec340260a7646cb341af694073fe1
BudgiePanic <bsanson90@gmail.com>
2024-02-23 15:19:09 +1300

    test: add tests for group ray intersections

--- src/test/java/com/BudgiePanic/rendering/util/shape/GroupTest.java
+++ src/test/java/com/BudgiePanic/rendering/util/shape/GroupTest.java
@@ -4,18 +4,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.junit.jupiter.api.Test;
 
-import com.BudgiePanic.rendering.util.Material;
 import static com.BudgiePanic.rendering.util.Tuple.makeVector;
 import static com.BudgiePanic.rendering.util.Tuple.makePoint;
-import com.BudgiePanic.rendering.util.Tuple;
-import com.BudgiePanic.rendering.util.intersect.Intersection;
 import com.BudgiePanic.rendering.util.intersect.Ray;
 import com.BudgiePanic.rendering.util.matrix.Matrix4;
+import com.BudgiePanic.rendering.util.transform.Transforms;
 
 public class GroupTest {
 
@@ -54,6 +49,45 @@ public class GroupTest {
         assertTrue(group.children.contains(shape));
         assertTrue(shape.parent.isPresent());
         assertEquals(group, shape.parent().get());
+    }
+
+    @Test
+    void testIntersectionEmptyGroup() {
+        var group = new Group(identity);
+        var ray = new Ray(makePoint(), makeVector(0, 0, 1));
+        var result = group.intersect(ray);
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    void testNonEmptyGroupIntersection() {
+        var group = new Group(identity);
+        Shape shape1 = new Sphere(identity),
+              shape2 = new Sphere(Transforms.identity().translate(0, 0, -3).assemble()),
+              shape3 = new Sphere(Transforms.identity().translate(5, 0, 0).assemble());
+        group.addShape(shape1);
+        group.addShape(shape2);
+        group.addShape(shape3);
+        var ray = new Ray(makePoint(0,0,-5), makeVector(0, 0, 1));
+        var result = group.localIntersect(ray);
+        assertTrue(result.isPresent());
+        var intersections = result.get();
+        assertEquals(4, intersections.size());
+        assertEquals(shape2, intersections.get(0).shape());
+        assertEquals(shape2, intersections.get(1).shape());
+        assertEquals(shape1, intersections.get(2).shape());
+        assertEquals(shape1, intersections.get(3).shape());
+    }
+
+    @Test
+    void testGroupTransform() {
+        var group = new Group(Transforms.identity().scale(2, 2, 2).assemble());
+        group.addShape(new Sphere(Transforms.identity().translate(5, 0, 0).assemble()));
+        var ray = new Ray(makePoint(10, 0, -10), makeVector(0, 0, 1));
+        var result = group.intersect(ray);
+        assertTrue(result.isPresent());
+        var intersections = result.get();
+        assertEquals(2, intersections.size());
     }
 
 }
