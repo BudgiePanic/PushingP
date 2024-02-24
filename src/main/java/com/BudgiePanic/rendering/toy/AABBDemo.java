5284b34eec7f2f7b5a66bbcf4adde0acbda3d7e5
BudgiePanic <bsanson90@gmail.com>
2024-02-25 12:26:08 +1300

    feat: add aabb performance demo to app

---
+++ src/main/java/com/BudgiePanic/rendering/toy/AABBDemo.java
@@ -0,0 +1,11 @@
+package com.BudgiePanic.rendering.toy;
+
+public class AABBDemo implements Runnable {
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'run'");
+    }
+    
+}
