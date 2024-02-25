a09637ba157db0ffb960f43cbba99c418310d5f9
BudgiePanic <bsanson90@gmail.com>
2024-02-25 16:49:21 +1300

    feat: add measuring camera
    the measuring camera times how long it takes for an image to be taken.

---
+++ src/main/java/com/BudgiePanic/rendering/reporting/TimingWrapper.java
@@ -0,0 +1,31 @@
+package com.BudgiePanic.rendering.reporting;
+
+import com.BudgiePanic.rendering.scene.Camera;
+import com.BudgiePanic.rendering.scene.World;
+import com.BudgiePanic.rendering.util.Canvas;
+import com.BudgiePanic.rendering.util.matrix.Matrix4;
+
+/**
+ * Wrapper class to non-intrusively measure the time it takes for a camera to take an image of a world.
+ * @author BudgiePanic
+ */
+public class TimingWrapper extends Camera {
+
+    // TODO: extract camera interface, hold a camera instance, instead of extending camera.
+    //       using a wrapper that holds a camera instance is a better design than extending camera...
+
+    public TimingWrapper(int width, int height, float fov, Matrix4 transform) {
+        super(width, height, fov, transform);
+    }
+
+    @Override
+    public Canvas takePicture(World world, Canvas canvas) {
+        System.out.println("INFO: timing duration of image capture");
+        var startTime = System.currentTimeMillis();
+        var result = super.takePicture(world, canvas);
+        var endTime = System.currentTimeMillis();
+        System.out.println("INFO: image of" + world + " via " + this.toString() + " took " + (endTime - startTime) + " milliseconds to complete.");
+        return result;
+    }
+    
+}
