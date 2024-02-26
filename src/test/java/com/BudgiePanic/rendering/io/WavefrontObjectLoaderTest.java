1937002dfb94e1a33fbe634f2f1fa5f66d6a8cd5
BudgiePanic <bsanson90@gmail.com>
2024-02-26 16:11:45 +1300

    test: add object file loader tests

---
+++ src/test/java/com/BudgiePanic/rendering/io/WavefrontObjectLoaderTest.java
@@ -0,0 +1,158 @@
+package com.BudgiePanic.rendering.io;
+
+import static com.BudgiePanic.rendering.util.Tuple.makePoint;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import com.BudgiePanic.rendering.util.shape.Triangle;
+
+public class WavefrontObjectLoaderTest {
+
+    @Test
+    void testParseJunkObject() {
+        var lines = List.of(
+            "well this clearly",
+            "is not what we would",
+            "expect to find in a wave",
+            "front object file now",
+            "is it?"
+        );
+        var result = WavefrontObjectLoader.parseObj(lines);
+        assertEquals(5, result.linesSkipped());
+    }
+
+    @Test
+    void testParseVertexData() {
+        // Should? note that the lists are 1 indexed, position 0 contains a dummy?
+        var lines = List.of(
+            "v -1 1 0",
+            "v -1.0000 0.5000 0.0000",
+            "v 1 0 0",
+            "v 1 1 0"
+        );
+        var result = WavefrontObjectLoader.parseObj(lines);
+        var verts = result.verticies();
+        assertEquals(5, verts.size());
+        assertEquals(null, verts.getFirst());
+        assertEquals(makePoint(-1, 1, 0), verts.get(1));
+        assertEquals(makePoint(-1, 0.5f, 0), verts.get(2));
+        assertEquals(makePoint(1, 0, 0), verts.get(3));
+        assertEquals(makePoint(1, 1, 0), verts.get(4));
+    }
+
+    @Test
+    void testParseTriangleData() {
+        var lines = List.of(
+            "v -1 1 0",
+            "v -1 0 0",
+            "v 1 0 0",
+            "v 1 1 0",
+            "",
+            "f 1 2 3",
+            "f 1 3 4"
+        );
+        var result = WavefrontObjectLoader.parseObj(lines);
+        var verts = result.verticies();
+        var triangles = result.triangles();
+        assertEquals(1, result.linesSkipped());
+        assertEquals(2, triangles);
+        assertEquals(verts.get(1), triangles.get(0).p1());
+        assertEquals(verts.get(2), triangles.get(0).p2());
+        assertEquals(verts.get(3), triangles.get(0).p3());
+        assertEquals(verts.get(1), triangles.get(1).p1());
+        assertEquals(verts.get(3), triangles.get(1).p2());
+        assertEquals(verts.get(4), triangles.get(1).p3());
+    }
+
+    @Test
+    void testPolygonTriangulation() {
+        var lines = List.of(
+            "v -1 1 0",
+            "v -1 0 0",
+            "v 1 0 0",
+            "v 1 1 0",
+            "v 0 2 0",
+            "",
+            "f 1 2 3 4 5"
+        );
+        var result = WavefrontObjectLoader.parseObj(lines);
+        var verts = result.verticies();
+        var triangles = result.triangles();
+        assertEquals(3, triangles);
+
+        assertEquals(verts.get(1), triangles.get(0).p1());
+        assertEquals(verts.get(2), triangles.get(0).p2());
+        assertEquals(verts.get(3), triangles.get(0).p3());
+
+        assertEquals(verts.get(1), triangles.get(1).p1());
+        assertEquals(verts.get(3), triangles.get(1).p2());
+        assertEquals(verts.get(4), triangles.get(1).p3());
+        
+        assertEquals(verts.get(1), triangles.get(2).p1());
+        assertEquals(verts.get(4), triangles.get(2).p2());
+        assertEquals(verts.get(5), triangles.get(2).p3());
+    }
+
+    @Test
+    void testParseNamesGroups() {
+        var lines = List.of(
+            "v -1 1 0",
+            "v -1 0 0",
+            "v 1 0 0",
+            "v 1 1 0",
+            "",
+            "g FirstGroup",
+            "f 1 2 3",
+            "g SecondGroup",
+            "f 1 3 4"
+        );
+        var result = WavefrontObjectLoader.parseObj(lines);
+        var verts = result.verticies();
+        assertEquals(2, result.groups().size());
+        var group1 = result.groups().get(0);
+        var group2 = result.groups().get(1);
+        assertEquals("FirstGroup", group1.a());
+        assertEquals("SecondGroup", group2.a());
+
+        var group1Shapes = group1.b().getShapes();
+        assertEquals(1, group1Shapes.size());
+        var triangle1 = (Triangle) group1Shapes.get(0);
+
+        var group2Shapes = group2.b().getShapes();
+        assertEquals(1, group2Shapes.size());
+        var triangle2 = (Triangle) group2Shapes.get(0);
+
+        assertEquals(verts.get(1), triangle1.p1());
+        assertEquals(verts.get(2), triangle1.p2());
+        assertEquals(verts.get(3), triangle1.p3());
+
+        assertEquals(verts.get(1), triangle2.p1());
+        assertEquals(verts.get(3), triangle2.p2());
+        assertEquals(verts.get(4), triangle2.p3());
+    }
+
+    @Test
+    void testUberGroupGeneration() {
+        var lines = List.of(
+            "v -1 1 0",
+            "v -1 0 0",
+            "v 1 0 0",
+            "v 1 1 0",
+            "",
+            "g FirstGroup",
+            "f 1 2 3",
+            "g SecondGroup",
+            "f 1 3 4"
+        );
+        var a = WavefrontObjectLoader.parseObj(lines);
+        var result = WavefrontObjectLoader.objectToGroup(a);
+        assertEquals(2, a.groups().size());
+        assertEquals(2, result.getShapes().size());
+        assertTrue(result.getShapes().contains(a.groups().get(0).b()));
+        assertTrue(result.getShapes().contains(a.groups().get(1).b()));
+    }    
+}
