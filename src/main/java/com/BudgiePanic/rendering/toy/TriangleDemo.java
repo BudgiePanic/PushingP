4c4712346b3430f057a289114e943155d7886203
BudgiePanic <bsanson90@gmail.com>
2024-02-26 19:03:23 +1300

    feat: add triangle demo to app

---
+++ src/main/java/com/BudgiePanic/rendering/toy/TriangleDemo.java
@@ -0,0 +1,10 @@
+package com.BudgiePanic.rendering.toy;
+
+public class TriangleDemo implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println("running triangle demo, this could take a while...");
+    }
+    
+}
