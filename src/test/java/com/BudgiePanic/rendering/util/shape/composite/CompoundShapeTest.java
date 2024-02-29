c2538f5c98a547522906b8e07a0dbae012f74f7d
BudgiePanic <bsanson90@gmail.com>
2024-02-29 17:03:55 +1300

    test: add shape filtering test

--- src/test/java/com/BudgiePanic/rendering/util/shape/composite/CompoundShapeTest.java
+++ src/test/java/com/BudgiePanic/rendering/util/shape/composite/CompoundShapeTest.java
@@ -2,8 +2,12 @@ package com.BudgiePanic.rendering.util.shape.composite;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
+import com.BudgiePanic.rendering.util.Pair;
+import com.BudgiePanic.rendering.util.intersect.Intersection;
 import com.BudgiePanic.rendering.util.matrix.Matrix4;
 import com.BudgiePanic.rendering.util.shape.Cube;
 import com.BudgiePanic.rendering.util.shape.Sphere;
@@ -20,6 +24,33 @@ public class CompoundShapeTest {
         assertEquals(shape2, shapeCompound.right());
         assertEquals(shapeCompound, shape1.parent().get());
         assertEquals(shapeCompound, shape2.parent().get());
+    }
+
+    @Test
+    void testCompoundShapeIntersectFiltering() {
+        var shape1 = new Sphere(Matrix4.identity());
+        var shape2 = new Cube(Matrix4.identity());
+        var tests = List.of(
+            new Pair<>(CompoundOperation.union, new Pair<>(0, 3)),
+            new Pair<>(CompoundOperation.intersect, new Pair<>(1, 2)),
+            new Pair<>(CompoundOperation.difference, new Pair<>(0, 1))
+        );
+        var dummyIntersections = List.of(
+            new Intersection(1f, shape1),
+            new Intersection(2f, shape2),
+            new Intersection(3f, shape1),
+            new Intersection(4f, shape2)
+        );
+        for (var test: tests) {
+            var cShape = new CompoundShape(test.a(), shape1, shape2, Matrix4.identity());
+            var result = cShape.filter(dummyIntersections);
+            assertEquals(2, result.size());
+            var resultA = result.get(0);
+            var resultB = result.get(1);
+            assertEquals(dummyIntersections.get(test.b().a()), resultA);
+            assertEquals(dummyIntersections.get(test.b().b()), resultB);
+        }
+        
     }
 
 }
