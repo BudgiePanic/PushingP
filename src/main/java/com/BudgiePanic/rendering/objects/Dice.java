9ff174ae6283074227f14e315e1338f5be5eb49d
BudgiePanic <50469074+BudgiePanic@users.noreply.github.com>
2024-03-08 14:54:21 +1300

    feat: finish compound shape demo (#77 via #85)
    * misc: remove dice default constructor
    
    it was only being used for manual testing
    
    * fix: make dice solid
    
    * fix: centre dimples on dice
    
    * fix: add missing space to logging message
    
    * feat: add dice to CSG demo
    
    * feat: add super csg shape to csg demo scene
    
    * feat: add yellow shape to scene
    
    * feat: add lenses to scene

--- src/main/java/com/BudgiePanic/rendering/objects/Dice.java
+++ src/main/java/com/BudgiePanic/rendering/objects/Dice.java
@@ -21,18 +21,28 @@ import com.BudgiePanic.rendering.util.transform.Transforms;
  */
 public class Dice extends CompoundShape {
     
-    public Dice() {
-        this(Transforms.identity().scale(1.0f).rotateY(AngleHelp.toRadians(-45f)).translate(0, 0, 0).assemble());
-    }
-
+    /**
+     * Creates a smooth dice wtih default materials
+     * @param transform
+     *   The dice transform, defines the dice's world position.
+     */
     public Dice(Matrix4 transform) {    
         this(transform, Material.defaultMaterial(), Material.defaultMaterial());
     }
 
+    /**
+     * Create a smooth dice.
+     * @param transform
+     *   The dice transform, defines the dice's world position.
+     * @param cubeMaterial
+     *   The material of the dice cube.
+     * @param dimpleMaterial
+     *   The material of the dice number dimples
+     */
     public Dice(Matrix4 transform, Material cubeMaterial, Material dimpleMaterial) {
         super(
             difference,
-            makeSmoothCube(Transforms.identity().translate(0.25f, 0.25f, 0.25f).scale(0.65f).assemble(), cubeMaterial),
+            makeSmoothCube(Transforms.identity().translate(0.30f, 0.30f, 0.30f).scale(0.65f).assemble(), cubeMaterial),
             makeDimples(dimpleMaterial),
             transform
         );
@@ -43,53 +53,53 @@ public class Dice extends CompoundShape {
         final var max = 1;
         final var min = 0;
         // cylinders
-        final var a = new Cylinder(Transforms.identity().scale(0.25f, 1.0f, 0.25f).rotateZ(-rotation).translate(0, 0, 0).assemble(), material, max, min) 
+        final var a = new Cylinder(Transforms.identity().scale(0.25f, 1.0f, 0.25f).rotateZ(-rotation).translate(0, 0, 0).assemble(), material, max, min, true) 
         {
             @Override public String toString() { return "a"; };
         }; 
-        final var b = new Cylinder(Transforms.identity().scale(0.25f, 1.0f, 0.25f).translate(1, 0, 0).rotateX(rotation).assemble(), material, max, min)
+        final var b = new Cylinder(Transforms.identity().scale(0.25f, 1.0f, 0.25f).translate(1, 0, 0).rotateX(rotation).assemble(), material, max, min, true)
         {
             @Override public String toString() { return "b"; };
         }; 
-        final var c = new Cylinder(Transforms.identity().scale(0.25f, 1.0f, 0.25f).translate(0, 0, 1).rotateZ(-rotation).assemble(), material, max, min) 
+        final var c = new Cylinder(Transforms.identity().scale(0.25f, 1.0f, 0.25f).translate(0, 0, 1).rotateZ(-rotation).assemble(), material, max, min, true) 
         {
             @Override public String toString() { return "c"; };
         };
-        final var d = new Cylinder(Transforms.identity().scale(0.25f, 1.0f, 0.25f).rotateX(rotation).translate(0, 0, 0).assemble(), material, max, min)
+        final var d = new Cylinder(Transforms.identity().scale(0.25f, 1.0f, 0.25f).rotateX(rotation).translate(0, 0, 0).assemble(), material, max, min, true)
         {
             @Override public String toString() { return "d"; };
         };
 
-        final var e = new Cylinder(Transforms.identity().scale(0.25f, 1.0f, 0.25f).translate(0, 0, 0).assemble(), material, max, min)
+        final var e = new Cylinder(Transforms.identity().scale(0.25f, 1.0f, 0.25f).translate(0, 0, 0).assemble(), material, max, min, true)
         {
             @Override public String toString() { return "e"; };
         }; 
-        final var f = new Cylinder(Transforms.identity().scale(0.25f, 1.0f, 0.25f).translate(1, 0, 0).assemble(), material, max, min)
+        final var f = new Cylinder(Transforms.identity().scale(0.25f, 1.0f, 0.25f).translate(1, 0, 0).assemble(), material, max, min, true)
         {
             @Override public String toString() { return "f"; };
         }; 
-        final var g = new Cylinder(Transforms.identity().scale(0.25f, 1.0f, 0.25f).translate(1, 0, 1).assemble(), material, max, min)
+        final var g = new Cylinder(Transforms.identity().scale(0.25f, 1.0f, 0.25f).translate(1, 0, 1).assemble(), material, max, min, true)
         {
             @Override public String toString() { return "g"; };
         }; 
-        final var h = new Cylinder(Transforms.identity().scale(0.25f, 1.0f, 0.25f).translate(0, 0, 1).assemble(), material, max, min)
+        final var h = new Cylinder(Transforms.identity().scale(0.25f, 1.0f, 0.25f).translate(0, 0, 1).assemble(), material, max, min, true)
         {
             @Override public String toString() { return "h"; };
         }; 
 
-        final var i = new Cylinder(Transforms.identity().scale(0.25f, 1.0f, 0.25f).rotateZ(-rotation).translate(0, 1, 0).assemble(), material, max, min)
+        final var i = new Cylinder(Transforms.identity().scale(0.25f, 1.0f, 0.25f).rotateZ(-rotation).translate(0, 1, 0).assemble(), material, max, min, true)
         {
             @Override public String toString() { return "i"; };
         }; 
-        final var j = new Cylinder(Transforms.identity().scale(0.25f, 1.0f, 0.25f).rotateX(rotation).translate(0, 1, 0).assemble(), material, max, min)
+        final var j = new Cylinder(Transforms.identity().scale(0.25f, 1.0f, 0.25f).rotateX(rotation).translate(0, 1, 0).assemble(), material, max, min, true)
         {
             @Override public String toString() { return "j"; };
         }; 
-        final var k = new Cylinder(Transforms.identity().scale(0.25f, 1.0f, 0.25f).rotateZ(-rotation).translate(0, 1, 1).assemble(), material, max, min)
+        final var k = new Cylinder(Transforms.identity().scale(0.25f, 1.0f, 0.25f).rotateZ(-rotation).translate(0, 1, 1).assemble(), material, max, min, true)
         {
             @Override public String toString() { return "k"; };
         }; 
-        final var l = new Cylinder(Transforms.identity().scale(0.25f, 1.0f, 0.25f).rotateX(rotation).translate(1, 1, 0).assemble(), material, max, min)
+        final var l = new Cylinder(Transforms.identity().scale(0.25f, 1.0f, 0.25f).rotateX(rotation).translate(1, 1, 0).assemble(), material, max, min, true)
         {
             @Override public String toString() { return "l"; };
         }; 
--- src/main/java/com/BudgiePanic/rendering/toy/CompoundShapeDemo.java
+++ src/main/java/com/BudgiePanic/rendering/toy/CompoundShapeDemo.java
@@ -2,6 +2,10 @@ package com.BudgiePanic.rendering.toy;
 
 import static com.BudgiePanic.rendering.util.Tuple.makePoint;
 import static com.BudgiePanic.rendering.util.Tuple.makeVector;
+import static com.BudgiePanic.rendering.util.matrix.Matrix4.identity;
+import static com.BudgiePanic.rendering.util.shape.composite.CompoundOperation.difference;
+import static com.BudgiePanic.rendering.util.shape.composite.CompoundOperation.intersect;
+import static com.BudgiePanic.rendering.util.shape.composite.CompoundOperation.union;
 
 import java.io.File;
 import java.io.IOException;
@@ -24,8 +28,12 @@ import com.BudgiePanic.rendering.util.pattern.BiOperation;
 import com.BudgiePanic.rendering.util.pattern.BiPattern;
 import com.BudgiePanic.rendering.util.pattern.Perturb;
 import com.BudgiePanic.rendering.util.pattern.SolidColor;
+import com.BudgiePanic.rendering.util.shape.Cube;
+import com.BudgiePanic.rendering.util.shape.Cylinder;
 import com.BudgiePanic.rendering.util.shape.Plane;
 import com.BudgiePanic.rendering.util.shape.Sphere;
+import com.BudgiePanic.rendering.util.shape.composite.CompoundShape;
+import com.BudgiePanic.rendering.util.shape.composite.Group;
 import com.BudgiePanic.rendering.util.transform.Transforms;
 import com.BudgiePanic.rendering.util.transform.View;
 
@@ -35,7 +43,7 @@ public class CompoundShapeDemo implements Runnable {
     private static final int width = 1536, height = width;
     private static final float fov = 90f;
     private final Tuple cameraPostion = makePoint(0, 3, -3);
-    private final Tuple cameraTarget = makePoint(0, 0, 1);
+    private final Tuple cameraTarget = makePoint(0, 0, 1.5f);
     // private final Camera camera = new TimingWrapper(width, height, fov, View.makeViewMatrix(makePoint(0, 5, 0), makePoint(0, 0, 10), makeVector(0, 1, 0)));
     private final Camera camera = new TimingWrapper(width, height, fov, View.makeViewMatrix(cameraPostion, cameraTarget, makeVector(0, 1, 0)));
 
@@ -43,7 +51,7 @@ public class CompoundShapeDemo implements Runnable {
     public void run() {
         World world = new World();
         // makePoint(-2, 8, -2)
-        world.addLight(new PointLight(makePoint(-2, 8, -2), Colors.white));
+        world.addLight(new PointLight(makePoint(-2, 6.5f, -2), Colors.white));
         world.addShape( // "sky box"
             new Sphere(
                 Transforms.identity().scale(15, 15, 15).assemble(),
@@ -63,11 +71,64 @@ public class CompoundShapeDemo implements Runnable {
                         new Perturb(new BiPattern(BiOperation.stripe, Colors.green.multiply(0.66f), Colors.blue.multiply(0.70f), 
                         Transforms.identity().rotateY(AngleHelp.toRadians(45f)).scale(0.5f, 0.5f, 0.5f).assemble()))
                     )
-                )
+                ).setReflectivity(0.06f)
             )
         );
-        world.addShape(new Dice());
-        // TODO we'll come back to this once #76 is fixed...
+        // the dice
+        world.addShape(new Dice( // RED BOTTOM
+            Transforms.identity().assemble(), 
+            Material.color(Colors.red.multiply(0.75f)), 
+            Material.defaultMaterial()));
+
+        world.addShape(new Dice( // GREEN BOTTOM
+            Transforms.identity().rotateY(AngleHelp.toRadians(101f)).translate(-1f, 0, 0.5f).assemble(), 
+            Material.color(Colors.green.multiply(0.18f)).setReflectivity(0.80f).setRefractiveIndex(1.5f).setTransparency(0.95f).setDiffuse(0.46f).setAmbient(0.06f).setShininess(500f), 
+            Material.defaultMaterial()));
+
+        world.addShape(new Dice( // BLUE TOP
+            Transforms.identity().rotateX(AngleHelp.toRadians(90f)).rotateY(-10f).translate(-0.23f, 2, 1f).assemble(),
+            Material.color(Colors.blue.multiply(0.75f)).setReflectivity(0.33f), 
+            Material.defaultMaterial().setReflectivity(0.10f)));
+        // super compound shape
+        final var factor = 0.33f;
+        final var mat = Material.color(Colors.blue.add(Colors.red).multiply(0.80f).add(Colors.green.multiply(0.2f))).setReflectivity(0.10f).setAmbient(0.30f);
+        world.addShape(new CompoundShape(difference, 
+          new CompoundShape(intersect, new Sphere(identity(), mat), new Cube(Transforms.identity().scale(0.70f).assemble(), mat), identity()),
+          new CompoundShape(union,
+              new CompoundShape(union, 
+                    new Cylinder(Transforms.identity().scale(factor,1,factor).assemble(), mat, 1, -1, true), 
+                    new Cylinder(Transforms.identity().scale(factor,1,factor).rotateZ(AngleHelp.toRadians(90f)).assemble(), mat, 1, -1, true), 
+                identity()),
+              new Cylinder(Transforms.identity().scale(factor,1,factor).rotateZ(AngleHelp.toRadians(90f)).rotateY(AngleHelp.toRadians(90f)).assemble(),mat, 1, -1, true), identity()), 
+          Transforms.identity().scale(0.5f).rotateY(AngleHelp.toRadians(60f)).rotateZ(AngleHelp.toRadians(30f)).rotateX(AngleHelp.toRadians(-45f)).translate(1f, 3f, 0.3f).assemble()
+        ));
+        // pac man
+        final var eyeLeftPos = new Tuple(-0.5f, 0.5f, 1f).normalize().multiply(1.2f);
+        final var eyeRightPos = new Tuple(-0.5f, -0.5f, 1f).normalize().multiply(1.2f);
+        final var pacManShapes = new Group(
+            Transforms.identity().scale(0.66f).rotateX(AngleHelp.toRadians(-90f)).rotateZ(AngleHelp.toRadians(90f)).rotateY(AngleHelp.toRadians(-110f)).rotateZ(AngleHelp.toRadians(180f)).rotateX(AngleHelp.toRadians(33f)).rotateY(AngleHelp.toRadians(-15f)).translate(2.5f, 2.5f, -0.66f).assemble()
+        );
+        pacManShapes.addShape(new CompoundShape(difference,
+        new CompoundShape(union, 
+            new Sphere(Transforms.identity().assemble(), Material.color(Colors.red.add(Colors.green))),
+            new CompoundShape(union, 
+                new Sphere(Transforms.identity().scale(0.1f).translate(eyeLeftPos.x, eyeLeftPos.y, eyeLeftPos.z).assemble(), Material.color(Colors.black).setReflectivity(0.10f)),
+                new Sphere(Transforms.identity().scale(0.1f).translate(eyeRightPos.x, eyeRightPos.y, eyeRightPos.z).assemble(), Material.color(Colors.black).setReflectivity(0.10f)),
+                Transforms.identity().assemble()),
+            Transforms.identity().assemble()),
+        new Cube(Transforms.identity().shear(0, 0, 0, 0, 1, 0).translate(1.1f, 0, 2.1f).assemble(), Material.color(Colors.black).setTransparency(1).setShadow(false)), identity()));
+        pacManShapes.addShape(new Sphere(Transforms.identity().scale(0.20f).translate(0.5f, 0, 1.15f).assemble(), Material.color(Colors.red.add(Colors.green))));
+        world.addShape(pacManShapes);
+        // The lenses
+        world.addShape(new CompoundShape(intersect, 
+            new Sphere(Transforms.identity().translate(0, 0, 0.5f).assemble(), Sphere.defaultGlassSphere().material()),
+            new Sphere(Transforms.identity().translate(0, 0, -0.5f).assemble(), Sphere.defaultGlassSphere().material()), 
+            Transforms.identity().rotateY(AngleHelp.toRadians(0f)).translate(-1, 1f, -1.8f).assemble()));
+        
+        world.addShape(new CompoundShape(intersect, 
+            new Sphere(Transforms.identity().translate(0, 0, 0.5f).assemble(), Sphere.defaultGlassSphere().material()),
+            new Sphere(Transforms.identity().translate(0, 0, -0.5f).assemble(), Sphere.defaultGlassSphere().material()), 
+            Transforms.identity().scale(0.25f).rotateY(AngleHelp.toRadians(0f)).translate(cameraPostion.x - 0.1f, cameraPostion.y + 0.2f, cameraPostion.z + 0.1f).assemble()));
 
 
         // ====== take the image ========
