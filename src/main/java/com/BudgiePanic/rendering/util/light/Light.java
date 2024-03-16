65904b3206cc01b3f432e5f32504463673282051
BudgiePanic <bsanson90@gmail.com>
2024-03-16 18:12:31 +1300

    feat: create light interface
    common operations all lights share

---
+++ src/main/java/com/BudgiePanic/rendering/util/light/Light.java
@@ -0,0 +1,34 @@
+package com.BudgiePanic.rendering.util.light;
+
+import com.BudgiePanic.rendering.scene.World;
+import com.BudgiePanic.rendering.util.Color;
+import com.BudgiePanic.rendering.util.Tuple;
+
+public interface Light {
+    /**
+     * Get the position of the light.
+     * @return
+     *   The position of the light.
+     */
+    Tuple position();
+
+    /**
+     * Get the color of the light.
+     *
+     * @return
+     *   The color of the light.
+     */
+    Color color();
+
+    /**
+     * How much light from this light is reaching the point in the world.
+     *
+     * @param point
+     *     The point being tested.
+     * @param world
+     *     The world the point is within.
+     * @return
+     *     The amount of light reaching the point.
+     */
+    float intensityAt(Tuple point, World world);
+}
