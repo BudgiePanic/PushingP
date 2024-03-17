e93573e14f96a083c170c3c1e8625e956caf5bfb
BudgiePanic <bsanson90@gmail.com>
2024-03-18 11:44:31 +1300

    misc: use direction vector helpers in demo

--- src/main/java/com/BudgiePanic/rendering/toy/AreaLightDemo.java
+++ src/main/java/com/BudgiePanic/rendering/toy/AreaLightDemo.java
@@ -1,11 +1,11 @@
 package com.BudgiePanic.rendering.toy;
 
 import static com.BudgiePanic.rendering.util.Tuple.makePoint;
-import static com.BudgiePanic.rendering.util.Tuple.makeVector;
 
 import com.BudgiePanic.rendering.scene.Camera;
 import com.BudgiePanic.rendering.scene.World;
 import com.BudgiePanic.rendering.util.Colors;
+import com.BudgiePanic.rendering.util.Directions;
 import com.BudgiePanic.rendering.util.Material;
 import com.BudgiePanic.rendering.util.light.AreaLight;
 import com.BudgiePanic.rendering.util.light.Light;
@@ -21,16 +21,16 @@ public class AreaLightDemo extends BaseDemo {
 
     @Override
     protected Camera getCamera() {
-        return new Camera(1280, 720, 90f, View.makeViewMatrix(makePoint(0, 2, -5), makePoint(0, 0, 10), makeVector(0, 1, 0)));
+        return new Camera(1280, 720, 90f, View.makeViewMatrix(makePoint(0, 2, -5), Directions.forward.multiply(10f), Directions.up));
     }
 
     @Override
     protected World createWorld() {
         System.out.println("INFO: running area light demo");
         var world = new World();
-        var light = (Light) new AreaLight(Colors.white.multiply(0.4f), makePoint(-3.5f, 3.5f, -3.5f), makeVector(0, 0, 1), makeVector(0, 1, 0), 8, 8, AreaLight.randomSamples);
+        var light = (Light) new AreaLight(Colors.white.multiply(0.4f), makePoint(-3.5f, 3.5f, -3.5f), Directions.forward, Directions.up, 8, 8, AreaLight.randomSamples);
         // light = new PointLight(light.position(), Colors.white);
-        world.addLight(new AreaLight(Colors.white.multiply(0.35f), makePoint(-1.5f, 10f, -15f), makeVector(1, 0, 0), makeVector(0, 1, 0), 6, 6, AreaLight.randomSamples));
+        world.addLight(new AreaLight(Colors.white.multiply(0.35f), makePoint(-1.5f, 10f, -15f), Directions.right, Directions.up, 6, 6, AreaLight.randomSamples));
         world.addLight(light);
         world.addShape(new Plane(Transforms.identity().translate(0, -0.5f, 0).assemble()));
         world.addShape(new Sphere(
