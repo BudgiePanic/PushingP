ba7e1e513f4070ee04ce9eb5f42df6afb0b9455d
BudgiePanic <bsanson90@gmail.com>
2024-03-21 20:57:20 +1300

    refactor: create depth camera write modes

--- src/main/java/com/BudgiePanic/rendering/scene/DepthCamera.java
+++ src/main/java/com/BudgiePanic/rendering/scene/DepthCamera.java
@@ -1,6 +1,7 @@
 package com.BudgiePanic.rendering.scene;
 
 import java.util.List;
+import java.util.function.Function;
 
 import com.BudgiePanic.rendering.util.Canvas;
 import com.BudgiePanic.rendering.util.Color;
@@ -15,11 +16,65 @@ import com.BudgiePanic.rendering.util.intersect.Ray;
  * @author BudgiePanic
  */
 public class DepthCamera implements Camera {
-
+    
+    /**
+     * Function to process the depth values before writing them to the depth buffer.
+     * TODO make this interface sealed, only permit normalized, raw, clamped
+     */
+    public static interface DepthMode extends Function<DepthCamera, Function<Color, Color>> {}
+    
+    /**
+     * Writes normalized depth values (between 1 and 0) to the depth buffer.
+     */
+    public static final DepthMode normalizedDepthValues = (camera) -> (pixel) -> {
+        final float distance = pixel.x;
+        final boolean isInfinity = Float.compare(distance, Float.POSITIVE_INFINITY) == 0;
+        final float normalizedDistance = isInfinity ? 0f : (distance - camera.minDistance) / (camera.maxDistance - camera.minDistance);
+        return new Color(normalizedDistance, normalizedDistance, normalizedDistance);
+    };
+    
+    /**
+     * Writes the raw depth values to the depth buffer.
+     */
+    public static final DepthMode rawDepthValues = (camera) -> (pixel) -> {
+        float distance = pixel.x;
+        final boolean isInfinity = Float.compare(distance, Float.POSITIVE_INFINITY) == 0;
+        distance = isInfinity ? 0f : distance;
+        return new Color(distance, distance, distance);
+    };
+    
+    /**
+     * Writes clamped depth values to the depth buffer. (depth values will be between 0 and max depth).
+     */
+    public static final DepthMode clampedDepthValues = (camera) -> (pixel) -> {
+        final float distance = pixel.x;
+        final boolean isInfinity = Float.compare(distance, Float.POSITIVE_INFINITY) == 0;
+        final float clampedDistance = isInfinity ? 0f : (distance) / (camera.maxDistance);
+        return new Color(clampedDistance, clampedDistance, clampedDistance);
+    };
+    
+    /**
+     * The camera that is being monitored. 
+     */
     protected final BasePerspectiveCamera cameraMonitoring;
+    /**
+     * How the depth values should be processed before being written to the depth buffer.
+     */
+    protected final DepthMode mode;
+    /**
+     * The furtherest distance to a ray intersection seen so far.
+     */
+    protected Float maxDistance;
+    /**
+     * The smallest distance to a ray-shape intersection seen so far.
+     */
+    protected Float minDistance;
 
-    public DepthCamera(BasePerspectiveCamera camera) {
+    public DepthCamera(BasePerspectiveCamera camera, DepthMode mode) {
         this.cameraMonitoring = camera;
+        this.mode = mode;
+        this.maxDistance = Float.MIN_VALUE;
+        this.minDistance = Float.MAX_VALUE;
     }
 
     @Override
@@ -47,11 +102,10 @@ public class DepthCamera implements Camera {
     }
 
     @Override
-    public Canvas takePicture(final World world, final Canvas canvas) {
+    public synchronized Canvas takePicture(final World world, final Canvas canvas) {
         if (canvas == null || canvas.getHeight() < cameraMonitoring.height || canvas.getWidth() < cameraMonitoring.width) throw new IllegalArgumentException();
         List<Pair<Integer, Integer>> jobs = cameraMonitoring.generateJobs();
-        // TODO code smell here: all camera's follow this same recipe for taking a picture, the only thing that changes is the consumer used in the stream
-        // TODO                  so this method can likely be pulled up, and concrete classes need only supply the consumer
+        // intersect each pixel-ray with the world and record the distance
         jobs.parallelStream().forEach(pixel -> {
             final int column = pixel.a();
             final int row = pixel.b();
@@ -60,7 +114,12 @@ public class DepthCamera implements Camera {
             final var distance = intersections.flatMap(Intersection::Hit).map(Intersection::a).orElse(Float.POSITIVE_INFINITY);
             final var inverseDistance = 1f / distance;
             canvas.writePixel(column, row, new Color(inverseDistance, inverseDistance, inverseDistance));
+            }
         });
+        canvas.writeAll(mode.apply(this));
+        // reset the distances in case this function is called again with a different world supplied
+        this.maxDistance = Float.MIN_VALUE; // max distance should be a local variable, but the lambdas can write to a local var...
+        this.minDistance = Float.MAX_VALUE;
         return canvas;
     }
     
