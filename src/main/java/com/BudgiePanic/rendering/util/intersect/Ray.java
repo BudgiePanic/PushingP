3d56a7c7cbe90c2a0b69d4f70edaab0d917abbaf
BudgiePanic <bsanson90@gmail.com>
2024-03-22 13:59:23 +1300

    refactor: add meta data to the ray
    rays now include when they were cast in the scene relative to the start of the image exposure time.
    this extra information will be used by motion shapes to help decide if the ray intersected with them

--- src/main/java/com/BudgiePanic/rendering/util/intersect/Ray.java
+++ src/main/java/com/BudgiePanic/rendering/util/intersect/Ray.java
@@ -8,12 +8,38 @@ import com.BudgiePanic.rendering.util.matrix.Matrix4;
  * 
  * @author BudgiePanic
  */
-public record Ray (Tuple origin, Tuple direction) {
+public record Ray (Tuple origin, Tuple direction, Float time) {
 
+    /**
+     * The default time the ray is cast, used if the convenience constructor is called.
+     */
+    protected static final Float defaultTime = Float.valueOf(0f);
+
+    /**
+     * Canonical ray constructor. Create a new ray.
+     * @param origin
+     *   The origin point of the ray.
+     * @param direction
+     *   The direction vector of the ray.
+     * @param time
+     *   The time when the ray was cast, measured from the start of the camera exposure.
+     */
     public Ray {
         if (origin == null || direction == null) throw new IllegalArgumentException("Ray cannot take null parameters");
         if (origin.isVector()) throw new IllegalArgumentException("origin should be point");
         if (direction.isPoint()) throw new IllegalArgumentException("direction should be vector");
+    }
+
+    /**
+     * Convenience constructor. Create a new ray cast at time zero.
+     *
+     * @param origin
+     *   The origin point of the ray.
+     * @param direction
+     *   The direction vector of the ray.
+     */
+    public Ray(Tuple origin, Tuple direction) {
+        this(origin, direction, defaultTime);
     }
 
     /**
@@ -37,6 +63,6 @@ public record Ray (Tuple origin, Tuple direction) {
      *   A new ray after it was passed through the matrix transform
      */
     public Ray transform(Matrix4 matrix) {
-        return new Ray(matrix.multiply(this.origin), matrix.multiply(this.direction()));
+        return new Ray(matrix.multiply(this.origin), matrix.multiply(this.direction()), this.time());
     }
 }
