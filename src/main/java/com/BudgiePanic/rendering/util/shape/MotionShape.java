41f3ebf2d58ec0d84a4ed76826562d1df88f230b
BudgiePanic <bsanson90@gmail.com>
2024-03-23 13:53:27 +1300

    refactor: remove force from motion shape
    if someone really wants their motion shape to exhibit non-linear motion they can make a subclass

--- src/main/java/com/BudgiePanic/rendering/util/shape/MotionShape.java
+++ src/main/java/com/BudgiePanic/rendering/util/shape/MotionShape.java
@@ -23,21 +23,6 @@ import com.BudgiePanic.rendering.util.transform.Translation;
 public class MotionShape extends BaseShape implements Parent {
 
     /**
-     * Force mode describes how the velocity should change during the exposure, if at all.
-     * For example, gravity will pull the object downwards, affecting the velocity of the shape.
-     */
-    public static interface ForceMode {}
-
-    /**
-     * Applies a downwards acceleration to the motion shape over the course of the exposure
-     */
-    public static final ForceMode gravity = new ForceMode() {};
-    /**
-     * Applies no acceleration force to the motion shape over the course of the exposure
-     */
-    public static final ForceMode none = new ForceMode() {};
-
-    /**
      * The time at which the object's motion starts.
      */
     protected static final float motionStartTime = 0f;
@@ -56,11 +41,6 @@ public class MotionShape extends BaseShape implements Parent {
      * The velocity of the shape at the start of the exposure.
      */
     protected final Tuple initialVelocity;
-
-    /**
-     * The force that acts on the motion shape over the course of the exposure, applying an acceleration.
-     */
-    protected final ForceMode force;
 
     /**
      * Cached AABB the encompases the shape for the duration of the motion.
@@ -82,33 +62,11 @@ public class MotionShape extends BaseShape implements Parent {
      *   The internal shape that is being moved by the motion
      * @param initialVelocity
      *   A vector that describes the direction the shape moves in units per second
-     * @param force
-     *   The forces, if any, that cause accelerations upon the shape, changing the velocity over time
-     */
-    public MotionShape(Matrix4 transform, Shape internal, Tuple initialVelocity, ForceMode force) { 
-        super(transform); 
-        this.shape = internal;
-        this.initialVelocity = initialVelocity;
-        this.force = force;
-        this.AABB = null;
-        this.motionEndTime = Optional.empty();
-    }
-
-    /**
-     * Creates a new motion shape with no forces acting upon it.
-     * 
-     * @param transform
-     *   The transform to get to object local space at time = 0s of the exposure
-     * @param internal 
-     *   The internal shape that is being moved by the motion
-     * @param initialVelocity
-     *   A vector that describes the direction the shape moves in units per second
      */
     public MotionShape(Matrix4 transform, Shape internal, Tuple initialVelocity) { 
         super(transform); 
         this.shape = internal;
         this.initialVelocity = initialVelocity;
-        this.force = none;
         this.AABB = null;
         this.motionEndTime = Optional.empty();
     }
@@ -132,8 +90,8 @@ public class MotionShape extends BaseShape implements Parent {
         // create a new ray that was moved by the offset amount, this is logically equivalent to moving the shape in the direction of the motion
         // we do this instead of transforming the shape because our shape's are immutable. 
         final float time = ray.time();
-        final Tuple offset = this.initialVelocity.multiply(time); // TODO defer this to the ForceMode, motion math won't be a straight line unless ForceMode is none
-        final Ray transformedRay = new Ray(ray.origin().add(offset.negate()), ray.direction()); // TODO should this tranformation be done here, in local object space? or in world space?
+        final Tuple offset = this.initialVelocity.multiply(time); 
+        final Ray transformedRay = new Ray(ray.origin().add(offset.negate()), ray.direction()); 
         // intersect the transformed ray against the child shape.
         final var mapper = Intersection.buildIntersector(transformedRay, inclusionCondition);
         final var intersections = mapper.apply(shape);
