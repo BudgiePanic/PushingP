b9bf79d72a7f5a640267cbe5a96303fe7398dab8
BudgiePanic <bsanson90@gmail.com>
2024-03-31 17:05:44 +1300

    feat: create linear camera wrapper
    computes each pixel one at a time instead of using multi threading

---
+++ src/main/java/com/BudgiePanic/rendering/reporting/ProceduralCameraWrapper.java
@@ -0,0 +1,53 @@
+package com.BudgiePanic.rendering.reporting;
+
+import java.util.List;
+
+import com.BudgiePanic.rendering.scene.Camera;
+import com.BudgiePanic.rendering.scene.World;
+import com.BudgiePanic.rendering.util.Canvas;
+import com.BudgiePanic.rendering.util.Color;
+import com.BudgiePanic.rendering.util.Pair;
+import com.BudgiePanic.rendering.util.intersect.Ray;
+
+/**
+ * A non-multithreaded camera. Used for end to end debugging.
+ *
+ * @author BudgiePanic
+ */
+public class ProceduralCameraWrapper implements Camera {
+
+    protected final Camera camera;
+
+    public ProceduralCameraWrapper(Camera camera) {
+        this.camera = camera;
+    }
+
+    @Override
+    public int width() { return camera.width(); }
+
+    @Override
+    public int height() { return camera.height(); }
+
+    @Override
+    public Ray createRay(int pixelColumn, int pixelRow, float time) { return camera.createRay(pixelColumn, pixelRow, time); }
+
+    @Override
+    public Color pixelAt(World world, int pixelColumn, int pixelRow, float time) { return camera.pixelAt(world, pixelColumn, pixelRow, time); }
+
+    @Override
+    public Color pixelAt(World world, int pixelColumn, int pixelRow) { return camera.pixelAt(world, pixelColumn, pixelRow); }
+
+    @Override 
+    public Canvas takePicture(World world, Canvas canvas) {
+        if (canvas == null || canvas.getHeight() < this.height() || canvas.getWidth() < this.width()) throw new IllegalArgumentException();
+        List<Pair<Integer, Integer>> jobs = generateJobs();
+        for (final var pixel : jobs) {
+            final int column = pixel.a();
+            final int row = pixel.b();
+            final var color = pixelAt(world, column, row);
+            canvas.writePixel(column, row, color);
+        }
+        return canvas;
+    }
+    
+}
