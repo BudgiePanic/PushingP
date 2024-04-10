b4485fd1898d2d227cdfc94b61ad387bfe397e80
BudgiePanic <bsanson90@gmail.com>
2024-04-10 14:14:18 +1200

    test: add dynamic sampler offset generator test

--- src/test/java/com/BudgiePanic/rendering/scene/SuperSamplingCameraTest.java
+++ src/test/java/com/BudgiePanic/rendering/scene/SuperSamplingCameraTest.java
@@ -5,11 +5,14 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
+import com.BudgiePanic.rendering.util.FloatHelp;
+
 public class SuperSamplingCameraTest {
     @Test
     void gridOutput() {
         assertDoesNotThrow(()->{
             var pattern = new SuperSamplingCamera.RotatedGrid(6);
+            pattern.cachedPoints.size();
             // print pattern result to test output file for manual inspection of results
             // pattern.cachedPoints.forEach(p -> System.out.println(p.a()));
             // System.out.println();
@@ -48,5 +51,17 @@ public class SuperSamplingCameraTest {
             assertTrue(p.b() >= 0, p.toString());
             assertTrue(p.b() <= 1, p.toString());
         });
+    }
+
+    @Test
+    void testDynamicSampler() {
+        int[] depths = {1,2,3,4,5};
+        float[] expecteds = {0.5f, 0.25f, 0.125f, 0.0625f, 0.03125f};
+        for (int i = 0; i < depths.length; i++) {
+            var depth = depths[i];
+            var expected = expecteds[i];
+            var result = SuperSamplingCamera.DynamicSampler.offset(depth);
+            assertTrue(FloatHelp.compareFloat(expected, result) == 0, "expected " + expected + " result " + result);
+        }
     }
 }
