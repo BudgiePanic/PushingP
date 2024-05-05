39d19b9c61560f6c4aae1b9a4f0c79876cbc076e
BudgiePanic <bsanson90@gmail.com>
2024-05-05 16:37:21 +1200

    refactor: move composite shape bounds logic
    moved the logic into BoundingBox::transform(Matrix)

--- src/main/java/com/BudgiePanic/rendering/util/shape/composite/CompositeShape.java
+++ src/main/java/com/BudgiePanic/rendering/util/shape/composite/CompositeShape.java
@@ -5,8 +5,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.function.Predicate;
 
-import static com.BudgiePanic.rendering.util.Tuple.makePoint;
-
 import com.BudgiePanic.rendering.util.Tuple;
 import com.BudgiePanic.rendering.util.intersect.Intersection;
 import com.BudgiePanic.rendering.util.intersect.Ray;
@@ -62,7 +60,7 @@ public abstract class CompositeShape extends BaseShape implements Parent {
      *   A list of ray-shape intersections made on the composite shapes children, if any.
      */
     protected final Optional<List<Intersection>> localIntersect(Ray ray, Predicate<Shape> condition) {
-        if (!bounds().intersect(ray)) { // AABB check
+        if (children().isEmpty() || !bounds().intersect(ray)) { // AABB check
             return Optional.empty();
         }
         return localIntersectI(ray, condition);
@@ -84,29 +82,19 @@ public abstract class CompositeShape extends BaseShape implements Parent {
     @Override
     public synchronized BoundingBox bounds() {
         if (AABB == null) {
-            BoundingBox box = new BoundingBox(makePoint(), makePoint()); // BUG HERE, fails when the MAX of the 8 points is a negative number
+            BoundingBox box = null;
             final var children = children();
             // the cube has 8 points [000,100,001,101,010,110,011,111]
             for (var shape : children) {
-                final BoundingBox localAABB = shape.bounds();
-                final var aabbMin = localAABB.minimum();
-                final var aabbMax = localAABB.maximum();
-                final var transform = shape.transform();
-                // find the AABB points in 'group space'
-                Tuple _000 = transform.multiply(new Tuple(aabbMax.x, aabbMin.y, aabbMin.z)); // MAX MIN 
-                Tuple _100 = transform.multiply(aabbMin); // MIN                                MIN MIN
-                Tuple _001 = transform.multiply(new Tuple(aabbMax.x, aabbMin.y, aabbMax.z)); // MAX MAX
-                Tuple _101 = transform.multiply(new Tuple(aabbMin.x, aabbMin.y, aabbMax.z)); // MIN MAX
-
-                Tuple _010 = transform.multiply(new Tuple(aabbMax.x, aabbMax.y, aabbMin.z)); // MAX MIN
-                Tuple _110 = transform.multiply(new Tuple(aabbMin.x, aabbMax.y, aabbMin.z)); // MIN MIN
-                Tuple _011 = transform.multiply(aabbMax); // MAX                                MAX MAX
-                Tuple _111 = transform.multiply(new Tuple(aabbMin.x, aabbMax.y, aabbMax.z)); // MIN MAX
-                var points = List.of(_000, _001, _010, _011, _100, _101, _110, _111);
-                // check if we need to grow the AABB extents to contain the points
-                for (var point : points) {
-                    if (!box.contains(point)) {
-                        box = box.grow(point);
+                final BoundingBox localAABB = shape.bounds().transform(shape.transform());
+                if (box == null) {
+                    box = localAABB;
+                } else {
+                    if (!box.contains(localAABB.maximum())) {
+                        box = box.grow(localAABB.maximum());
+                    }
+                    if (!box.contains(localAABB.minimum())) {
+                        box = box.grow(localAABB.minimum());
                     }
                 }
             }
