e36ab5b60c3d76401410569641012cc1f8086916
BudgiePanic <bsanson90@gmail.com>
2024-05-07 15:30:12 +1200

    refactor: add super sample camera to complex scene

--- src/main/java/com/BudgiePanic/rendering/toy/BigSceneDemo.java
+++ src/main/java/com/BudgiePanic/rendering/toy/BigSceneDemo.java
@@ -7,12 +7,15 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.List;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 import org.apache.commons.io.FileUtils;
 
 import com.BudgiePanic.rendering.io.WavefrontObjectLoader;
+import com.BudgiePanic.rendering.reporting.TimingWrapper;
 import com.BudgiePanic.rendering.scene.Camera;
 import com.BudgiePanic.rendering.scene.PinHoleCamera;
+import com.BudgiePanic.rendering.scene.SuperSamplingCamera;
 import com.BudgiePanic.rendering.scene.World;
 import com.BudgiePanic.rendering.util.Color;
 import com.BudgiePanic.rendering.util.Colors;
@@ -33,14 +36,17 @@ public class BigSceneDemo extends BaseDemo {
     /**
      * The number of shapes that can go into a leaf node in the bounding volume hierarchy
      */
-    //int threshold = 30;
+    int threshold = 30;
+
+    double modelHeightOffset = 0.03; // not sure why our scene does not line up with the book author's scene description in the bonus chapter.// maybe we need to translate then scale? looks close enough
 
     @Override
     protected String getName() { return "complex_scene.ppm"; }
 
     @Override
-    protected Camera getCamera() {
-        return new PinHoleCamera(500, 200, 1.2, View.makeViewMatrix(makePoint(0, 2.5, -10), makePoint(0, 1, 0), Directions.up));
+    protected Camera getCamera() { /* 1200 by 480 */
+        return new TimingWrapper(new SuperSamplingCamera(new PinHoleCamera(1200, 480, 1.2, View.makeViewMatrix(makePoint(0, 2.5, -10), makePoint(0, 1, 0), Directions.up)), SuperSamplingCamera.grid));
+        // return new TimingWrapper(new PinHoleCamera(1200, 480, 1.2, View.makeViewMatrix(makePoint(0, 2.5, -10), makePoint(0, 1, 0), Directions.up)));
     }
 
     protected List<String> loadModel() {
@@ -71,16 +77,16 @@ public class BigSceneDemo extends BaseDemo {
             final var transform = params.b();
             final var modelData = WavefrontObjectLoader.parseObj(lines, material);
             final var model = WavefrontObjectLoader.objectToGroup(modelData, transform);
-            model.divide(30);
+            model.divide(threshold);
             return model; 
         };
         var boxTransform = Transforms.identity().
-        translate(-3.9863, -0.1217, -1.1820).
-        scale(3.73335, 2.5845, 1.6283).
         translate(1, 1, 1).
+        scale(3.73335, 2.5845, 1.6283).
+        translate(-3.9863, -0.1217, -1.1820).
         scale(0.268).
-        translate(0, 0.1217, 0)
-        .assemble();
+        translate(0, modelHeightOffset, 0).
+        assemble();
         var box1 = new Cube(
             boxTransform,
             Material.color(Colors.white).
@@ -101,6 +107,10 @@ public class BigSceneDemo extends BaseDemo {
             setRefractiveIndex(1).
             setShadow(false)
         );
+        var box2A = new Cube(
+            boxTransform, 
+            box2.material()
+        );
         var box3 = new Cube(
             boxTransform, 
             Material.color(Colors.white).
@@ -111,82 +121,88 @@ public class BigSceneDemo extends BaseDemo {
             setRefractiveIndex(1).
             setShadow(false)
         );
+        var box3A = new Cube(
+            boxTransform, 
+            box3.material()
+        );
         
-        var cylinder = new Cylinder(Transforms.identity().assemble(), 
-        Material.color(Colors.white.multiply(0.2)).setAmbient(0).setDiffuse(0.8).setSpecular(0).setReflectivity(0.2),
-        0, -0.15, true);
+        Supplier<Shape> makeCylinder = () -> {
+            return new Cylinder(Transforms.identity().translate(0, 0, 0).assemble(), 
+            Material.color(Colors.white.multiply(0.2)).setAmbient(0).setDiffuse(0.8).setSpecular(0).setReflectivity(0.2),
+            0, -0.15, true);
+        };
 
         var groupA = new Group(Transforms.identity().translate(0, 2, 0).assemble());
-        groupA.addShape(cylinder);
+        groupA.addShape(makeCylinder.get());
         var subGroupA = new Group(Transforms.identity().assemble());
         subGroupA.addShape(box1);
         subGroupA.addShape(buildDragon.apply(
             new Pair<Material,Matrix4>(
                 Material.color(new Color(1, 0, 0.1)).setDiffuse(0.6).setSpecular(0.3).setShininess(15), 
-                Transforms.identity().scale(0.268).translate(0, 0.1217, 0).assemble())
+                Transforms.identity().scale(0.268).translate(0, modelHeightOffset, 0).assemble())
             )
         );
         groupA.addShape(subGroupA);
         world.addShape(groupA);
 
         var groupB = new Group(Transforms.identity().translate(2, 1, -1).assemble());
-        groupB.addShape(cylinder);
+        groupB.addShape(makeCylinder.get());
         var subGroupB = new Group(Transforms.identity().scale(0.75).rotateY(4).assemble());
         subGroupB.addShape(box2);
         subGroupB.addShape(buildDragon.apply(
             new Pair<Material,Matrix4>(
                 Material.color(new Color(1, 0.5, 0.1)).setDiffuse(0.6).setSpecular(0.3).setShininess(15), 
-                Transforms.identity().scale(0.268).translate(0, 0.1217, 0).assemble())
+                Transforms.identity().scale(0.268).translate(0, modelHeightOffset, 0).assemble())
             )
         );
         groupB.addShape(subGroupB);
         world.addShape(groupB);
 
         var groupC = new Group(Transforms.identity().translate(-2, 0.75, -1).assemble());
-        groupC.addShape(cylinder);
+        groupC.addShape(makeCylinder.get());
         var subGroupC = new Group(Transforms.identity().scale(0.75).rotateY(-0.4).assemble());
-        subGroupC.addShape(box2);
+        subGroupC.addShape(box2A);
         subGroupC.addShape(buildDragon.apply(
             new Pair<Material,Matrix4>(
                 Material.color(new Color(0.9, 0.5, 0.1)).setDiffuse(0.6).setSpecular(0.3).setShininess(15), 
-                Transforms.identity().scale(0.268).translate(0, 0.1217, 0).assemble())
+                Transforms.identity().scale(0.268).translate(0, modelHeightOffset, 0).assemble())
             )
         );
         groupC.addShape(subGroupC);
         world.addShape(groupC);
 
         var groupD = new Group(Transforms.identity().translate(-4, 0, -2).assemble());
-        groupD.addShape(cylinder);
+        groupD.addShape(makeCylinder.get());
         var subGroupD = new Group(Transforms.identity().scale(0.5).rotateY(-0.2).assemble());
         subGroupD.addShape(box3);
         subGroupD.addShape(buildDragon.apply(
             new Pair<Material,Matrix4>(
                 Material.color(new Color(1, 0.9, 0.1)).setDiffuse(0.6).setSpecular(0.3).setShininess(15), 
-                Transforms.identity().scale(0.268).translate(0, 0.1217, 0).assemble())
+                Transforms.identity().scale(0.268).translate(0, modelHeightOffset, 0).assemble())
             )
         );
         groupD.addShape(subGroupD);
         world.addShape(groupD);
 
         var groupE = new Group(Transforms.identity().translate(4, 0, -2).assemble());
-        groupE.addShape(cylinder);
+        groupE.addShape(makeCylinder.get());
         var subGroupE = new Group(Transforms.identity().scale(0.5).rotateY(3.3).assemble());
-        subGroupE.addShape(box3);
+        subGroupE.addShape(box3A);
         subGroupE.addShape(buildDragon.apply(
             new Pair<Material,Matrix4>(
                 Material.color(new Color(0.9, 1, 0.1)).setDiffuse(0.6).setSpecular(0.3).setShininess(15), 
-                Transforms.identity().scale(0.268).translate(0, 0.1217, 0).assemble())
+                Transforms.identity().scale(0.268).translate(0, modelHeightOffset, 0).assemble())
             )
         );
         groupE.addShape(subGroupE);
         world.addShape(groupE);
 
         var groupF = new Group(Transforms.identity().translate(0, 0.5, -4).assemble());
-        groupF.addShape(cylinder);
+        groupF.addShape(makeCylinder.get());
         groupF.addShape(buildDragon.apply(
             new Pair<Material,Matrix4>(
                 Material.color(Colors.white).setDiffuse(0.6).setSpecular(0.3).setShininess(15), 
-                Transforms.identity().scale(0.268).translate(0, 0.1217, 0).rotateY(3.1415).assemble())
+                Transforms.identity().scale(0.268).translate(0, modelHeightOffset, 0).rotateY(3.1415).assemble())
             )
         );
         world.addShape(groupF);
