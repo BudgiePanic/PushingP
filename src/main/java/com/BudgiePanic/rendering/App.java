3f313fb05b37588fffde3b79808b423c3dfb31fe
BudgiePanic <50469074+BudgiePanic@users.noreply.github.com>
2024-05-18 18:48:34 +1200

    feat: add normal bumping function to material (#91 via #113)
    * feat: add normal bumping function to material
    
    * feat: pass normal vector through bumping function
    
    * feat: add normal bump demo
    
    * docs: add missing lerper definition

--- src/main/java/com/BudgiePanic/rendering/App.java
+++ src/main/java/com/BudgiePanic/rendering/App.java
@@ -38,7 +38,8 @@ public class App
         entry("-anti_alias_demo", ()-> new AntiAliasingDemo()),
         entry("-bvh_test", ()-> new BoundingVolumeHierarchyTest()),
         entry("-dragon_demo", ()-> new LargeModelDemo()),
-        entry("-big_scene_demo", ()-> new BigSceneDemo())
+        entry("-big_scene_demo", ()-> new BigSceneDemo()),
+        entry("-normal_bump_demo", ()-> new NormalBumpDemo())
     );
 
     public static void main( String[] args )
