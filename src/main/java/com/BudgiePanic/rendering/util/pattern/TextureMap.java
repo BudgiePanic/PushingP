d2966a02bd954a985147c19496f46ead161b5f2e
BudgiePanic <bsanson90@gmail.com>
2024-05-19 21:33:37 +1200

    feat: implement texture mapping pattern

---
+++ src/main/java/com/BudgiePanic/rendering/util/pattern/TextureMap.java
@@ -0,0 +1,43 @@
+package com.BudgiePanic.rendering.util.pattern;
+
+import com.BudgiePanic.rendering.util.Color;
+import com.BudgiePanic.rendering.util.Tuple;
+import com.BudgiePanic.rendering.util.matrix.Matrix4;
+
+/**
+ * A pattern that uses texture mapping.
+ *
+ * @author BudgiePanic
+ */
+public record TextureMap(Pattern2D pattern, CoordinateMapper mapper, Matrix4 transform) implements Pattern {
+
+    /**
+     * Canonical constructor.
+     * @param pattern
+     *   The 2D pattern to sample.
+     * @param mapper
+     *   The coordinate system transfer function.
+     * @param transform
+     *   The pattern space transform.
+     */
+    public TextureMap {}
+
+    /**
+     * Convenience constructor. Uses the identity transform.
+     * @param pattern
+     *   The 2D pattern to sample.
+     * @param mapper
+     *   The pattern space transform.
+     */
+    public TextureMap(Pattern2D pattern, CoordinateMapper mapper) {
+        this(pattern, mapper, Matrix4.identity());
+    }
+
+    @Override
+    public Color colorAt(Tuple point) {
+        final double u = mapper.uSphereMap(point);
+        final double v = mapper.vSphereMap(point);
+        return pattern.sample(u, v);
+    }
+
+}
