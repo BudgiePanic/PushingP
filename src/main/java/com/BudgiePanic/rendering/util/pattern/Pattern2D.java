bd8d558694bbd477b3b767883adc789642a9cbf6
BudgiePanic <bsanson90@gmail.com>
2024-05-25 22:34:09 +1200

    feat: add texture map test pattern

--- src/main/java/com/BudgiePanic/rendering/util/pattern/Pattern2D.java
+++ src/main/java/com/BudgiePanic/rendering/util/pattern/Pattern2D.java
@@ -43,6 +43,34 @@ public interface Pattern2D {
     public static Pattern2D solidColor(final Color color) { return (u,v) -> { return color; }; }
 
     /**
+     * Verification pattern used to check if a shape TextureMap object is working correctly.
+     * @param background
+     *   The background color of the pattern
+     * @param upperLeft
+     *   The color in the upper left corner
+     * @param upperRight
+     *   The color in the upper right corner
+     * @param lowerLeft
+     *   The color in the lower left corner
+     * @param lowerRight
+     *   The color in the lower right corner
+     * @return
+     *   A pattern with different colors in each corner
+     */
+    public static Pattern2D mapCheck(Color background, Color upperLeft, Color upperRight, Color lowerLeft, Color lowerRight) { 
+        return (final double u, final double v) -> {
+            if (v > 0.8) {
+                if (u < 0.2) { return upperLeft; }
+                if (u > 0.8) { return upperRight; }
+            } else if (v < 0.2) {
+                if (u < 0.2) { return lowerLeft; }
+                if (u > 0.8) { return lowerRight; }
+            }
+            return background;
+        }; 
+    }
+
+    /**
      * Sample the 2D pattern.
      *
      * @param u
