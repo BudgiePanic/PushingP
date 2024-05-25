0adc904824da3e378e415d4dfca77a48d3084ed5
BudgiePanic <bsanson90@gmail.com>
2024-05-25 22:34:21 +1200

    test: add texture map test pattern tests

--- src/test/java/com/BudgiePanic/rendering/util/pattern/Pattern2DTest.java
+++ src/test/java/com/BudgiePanic/rendering/util/pattern/Pattern2DTest.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
+import com.BudgiePanic.rendering.util.Color;
 import com.BudgiePanic.rendering.util.Colors;
 import com.BudgiePanic.rendering.util.Pair;
 
@@ -42,6 +43,26 @@ public class Pattern2DTest {
                 var result = pattern.sample(x, y);
                 assertEquals(Colors.blue, result, "x " + x + " y " + y);
             }
+        }
+    }
+
+    @Test
+    void testMapCheckPattern() {
+        Color a = Colors.white, b = Colors.red, c = new Color(1,1,0), d = Colors.green, e = new Color(0,1,1);
+        var pattern = Pattern2D.mapCheck(a, b, c, d, e);
+        var tests = List.of(
+            new Pair<>(new double[]{0.5,0.5}, a),
+            new Pair<>(new double[]{0.1,0.9}, b),
+            new Pair<>(new double[]{0.9,0.99}, c),
+            new Pair<>(new double[]{0.1,0.1}, d),
+            new Pair<>(new double[]{0.9,0.1}, e)
+        );
+        for (final var test : tests) {
+            var u = test.a()[0];
+            var v = test.a()[1];
+            var expected = test.b();
+            var result = pattern.sample(u, v);
+            assertEquals(expected, result);
         }
     }
 }
