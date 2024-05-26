01fe1580fb5702828c5015e4435e33053d11f5f3
BudgiePanic <bsanson90@gmail.com>
2024-05-26 17:47:14 +1200

    misc: add texture mapped cubes to the texture demo

--- src/main/java/com/BudgiePanic/rendering/toy/TextureMapDemo.java
+++ src/main/java/com/BudgiePanic/rendering/toy/TextureMapDemo.java
@@ -6,6 +6,7 @@ import com.BudgiePanic.rendering.scene.Camera;
 import com.BudgiePanic.rendering.scene.PinHoleCamera;
 import com.BudgiePanic.rendering.scene.World;
 import com.BudgiePanic.rendering.util.AngleHelp;
+import com.BudgiePanic.rendering.util.Color;
 import com.BudgiePanic.rendering.util.Colors;
 import com.BudgiePanic.rendering.util.Directions;
 import com.BudgiePanic.rendering.util.Material;
@@ -13,8 +14,10 @@ import com.BudgiePanic.rendering.util.light.PointLight;
 import com.BudgiePanic.rendering.util.pattern.BiOperation;
 import com.BudgiePanic.rendering.util.pattern.BiPattern;
 import com.BudgiePanic.rendering.util.pattern.CoordinateMapper;
+import com.BudgiePanic.rendering.util.pattern.CubeTextureMap;
 import com.BudgiePanic.rendering.util.pattern.Pattern2D;
 import com.BudgiePanic.rendering.util.pattern.TextureMap;
+import com.BudgiePanic.rendering.util.shape.Cube;
 import com.BudgiePanic.rendering.util.shape.Cylinder;
 import com.BudgiePanic.rendering.util.shape.Plane;
 import com.BudgiePanic.rendering.util.shape.Sphere;
@@ -28,41 +31,50 @@ public class TextureMapDemo extends BaseDemo {
 
     @Override
     protected Camera getCamera() {
-        return new PinHoleCamera(500, 500, AngleHelp.toRadians(60), View.makeViewMatrix(makePoint(0,0,-3), makePoint(0, 0, 0), Directions.up));
+        return new PinHoleCamera(800, 400, 0.8, View.makeViewMatrix(makePoint(0,0, -20), makePoint(0, 0, 0), Directions.up));
     }
 
     @Override
     protected World createWorld() {
         World world = new World();
 
-        world.addLight(new PointLight(makePoint(-3, 5, -10), Colors.white));
-        world.addShape(new Plane(Transforms.identity().rotateX(AngleHelp.toRadians(-70)).rotateY(AngleHelp.toRadians(10)).translate(0, -3, 2).assemble(), 
-            Material.pattern(
-                new TextureMap(
-                    // Pattern2D.checker(2, 2, Pattern2D.solidColor(Colors.green), Pattern2D.solidColor(Colors.white)),
-                    Pattern2D.mapCheck(Colors.white, Colors.red, Colors.green, Colors.blue, Colors.blue.add(Colors.red)),
-                    CoordinateMapper.planar, Transforms.identity().assemble()
-                )
-            )
-            //Material.pattern(new BiPattern(BiOperation.checker, Colors.green, Colors.white))
-        ));
-        world.addShape(new Cylinder(Transforms.identity().scale(1, Math.PI, 1).translate(0, -0.5, 0).rotateX(Math.PI/2).assemble(), 
-            Material.pattern(
-                new TextureMap(
-                    Pattern2D.checker(16, 8, Pattern2D.solidColor(Colors.green), Pattern2D.solidColor(Colors.white)),
-                    CoordinateMapper.cylindircal, Transforms.identity().assemble()
-                )
-            ), 1.0, 0.0, true
-        ));
-        world.addShape(new Sphere(Transforms.identity().rotateY(AngleHelp.toRadians(15)).assemble(), 
-            Material.pattern(
-                new TextureMap(
-                    Pattern2D.checker(30, 10, Pattern2D.solidColor(Colors.green), Pattern2D.solidColor(Colors.white)),
-                    CoordinateMapper.sphere, Transforms.identity().assemble() // translation can affect the resulting checker pattern
-                )
-            )
-        ));
+        world.addLight(new PointLight(makePoint(0, 100, -100), Colors.white.multiply(0.25)));
+        world.addLight(new PointLight(makePoint(0, -100, -100), Colors.white.multiply(0.25)));
+        world.addLight(new PointLight(makePoint(-100, 0, -100), Colors.white.multiply(0.25)));
+        world.addLight(new PointLight(makePoint(100, 0, -100), Colors.white.multiply(0.25)));
+        
+        
+        final Color red = Colors.red, yellow = new Color(1, 1, 0), brown = new Color(1,0.5,0), 
+        green = Colors.green, cyan = new Color(0,1,1), blue = Colors.blue, purple = new Color(1, 0, 1),
+        white = Colors.white;
+        final var left = Pattern2D.mapCheck(yellow, cyan, red, blue, brown);
+        final var front = Pattern2D.mapCheck(cyan, red, yellow, brown, green);
+        final var right = Pattern2D.mapCheck(red, yellow, purple, green, white);
+        final var back = Pattern2D.mapCheck(green, purple, cyan, white, blue);
+        final var up = Pattern2D.mapCheck(brown, cyan, purple, red, yellow);
+        final var down = Pattern2D.mapCheck(purple, brown, green, blue, white);
+        final var cube = new CubeTextureMap(front, left, right, up, down, back);
 
+        final Material material = new Material(
+            cube,
+            0.2, 0.8, 0, Material.defaultShininess, Material.defaultReflectivity, 
+            Material.defaultTransparency, Material.defaultRefractiveIndex, 
+            Material.defaultShadowCast, Material.defaultNormalBump
+        );
+
+        Cube[] cubes = new Cube[] {
+            new Cube(Transforms.identity().rotateY(0.7854).rotateX(0.7854).translate(-6, 2, 0).assemble(), material),
+            new Cube(Transforms.identity().rotateY(2.3562).rotateX(0.7854).translate(-2, 2, 0).assemble(), material),
+            new Cube(Transforms.identity().rotateY(3.927).rotateX(0.7854).translate(2, 2, 0).assemble(), material),
+            new Cube(Transforms.identity().rotateY(5.4978).rotateX(0.7854).translate(6, 2, 0).assemble(), material),
+            new Cube(Transforms.identity().rotateY(0.7854).rotateX(-0.7854).translate(-6, -2, 0).assemble(), material),
+            new Cube(Transforms.identity().rotateY(2.3562).rotateX(-0.7854).translate(-2, -2, 0).assemble(), material),
+            new Cube(Transforms.identity().rotateY(3.927).rotateX(-0.7854).translate(2, -2, 0).assemble(), material),
+            new Cube(Transforms.identity().rotateY(5.4978).rotateX(-0.7854).translate(6, -2, 0).assemble(), material)
+        };
+        for (Cube box : cubes) {
+            world.addShape(box);
+        }
         return world;
     }
     
