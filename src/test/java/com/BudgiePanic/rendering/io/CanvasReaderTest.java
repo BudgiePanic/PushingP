cbc64d7adcab28d685b151d96afb3f20385cc50f
BudgiePanic <bsanson90@gmail.com>
2024-06-04 22:53:28 +1200

    refactor: remove unused imports

--- src/test/java/com/BudgiePanic/rendering/io/CanvasReaderTest.java
+++ src/test/java/com/BudgiePanic/rendering/io/CanvasReaderTest.java
@@ -2,9 +2,7 @@ package com.BudgiePanic.rendering.io;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 
