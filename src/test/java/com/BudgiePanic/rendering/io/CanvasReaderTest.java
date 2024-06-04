fa10a3274036b9b3c865bf98730f32e761d8074a
BudgiePanic <bsanson90@gmail.com>
2024-06-04 22:46:43 +1200

    test: add more parser tests

--- src/test/java/com/BudgiePanic/rendering/io/CanvasReaderTest.java
+++ src/test/java/com/BudgiePanic/rendering/io/CanvasReaderTest.java
@@ -10,6 +10,9 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
+import com.BudgiePanic.rendering.util.Color;
+import com.BudgiePanic.rendering.util.Pair;
+
 public class CanvasReaderTest {
     @Test
     void testCheckMagic() {
@@ -29,4 +32,66 @@ public class CanvasReaderTest {
         });
         assertThrows(CanvasReader.ParsingException.class, () -> { parser.consumeLine("not a line"); });
     }
+
+    @Test
+    void testCanvasReaderA() {
+        assertDoesNotThrow(() -> {
+            var result = CanvasReader.parseLines(List.of("P3","10 2","255",
+            "0 0 0  0 0 0  0 0 0  0 0 0  0 0 0",
+            "0 0 0  0 0 0  0 0 0  0 0 0  0 0 0",
+            "0 0 0  0 0 0  0 0 0  0 0 0  0 0 0",
+            "0 0 0  0 0 0  0 0 0  0 0 0  0 0 0"));
+            assertEquals(10, result.getWidth());
+            assertEquals(2, result.getHeight());
+        });
+    }
+
+    @Test
+    void testCanvasReaderB() {
+        assertDoesNotThrow(() -> {
+            var result = CanvasReader.parseLines(List.of("P3","4 3","255",
+            "255 127 0  0 127 255  127 255 0  255 255 255",
+            "0 0 0  255 0 0  0 255 0  0 0 255",
+            "255 255 0  0 255 255  255 0 255  127 127 127"));
+            assertEquals(4, result.getWidth());
+            assertEquals(3, result.getHeight());
+
+            var tests = List.of(
+                new Pair<>(new Pair<>(0, 0), new Color(1, 0.498, 0)),
+                new Pair<>(new Pair<>(1, 0), new Color(0, 0.498, 1)),
+                new Pair<>(new Pair<>(2, 0), new Color(0.498, 1, 0)),
+                new Pair<>(new Pair<>(3, 0), new Color(1, 1, 1)),
+                new Pair<>(new Pair<>(0, 1), new Color(0, 0, 0)),
+                new Pair<>(new Pair<>(1, 1), new Color(1, 0, 0)),
+                new Pair<>(new Pair<>(2, 1), new Color(0, 1, 0)),
+                new Pair<>(new Pair<>(3, 1), new Color(0, 0, 1)),
+                new Pair<>(new Pair<>(0, 2), new Color(1, 1, 0)),
+                new Pair<>(new Pair<>(1, 2), new Color(0, 1, 1)),
+                new Pair<>(new Pair<>(2, 2), new Color(1, 0, 1)),
+                new Pair<>(new Pair<>(3, 2), new Color(0.498, 0.498, 0.498))
+            );
+
+            for (final var test : tests) {
+                var expected = test.b();
+                var actual = result.getPixel(test.a().a(), test.a().b());
+                assertEquals(expected, actual);
+            }
+        });
+    }
+
+    @Test
+    void testCommentParsing() {
+        assertDoesNotThrow(() -> {
+            var result = CanvasReader.parseLines(List.of("P3", "# comment","2 1","#another comment", "255", "#",
+            "255 255 255",
+            "# decoy",
+            "255 0 255"));
+            assertEquals(2, result.getWidth());
+            assertEquals(1, result.getHeight());
+
+            assertEquals(new Color(1, 1, 1), result.getPixel(0, 0));
+            assertEquals(new Color(1, 0, 1), result.getPixel(1, 0));
+        });
+    }
 }
+ 
