a9a9fa1042641e2adec1752da6084ceb3650eb77
BudgiePanic <bsanson90@gmail.com>
2024-06-05 11:04:38 +1200

    feat: implement texture sampling function

---
+++ src/main/java/com/BudgiePanic/rendering/util/pattern/Texture2D.java
@@ -0,0 +1,19 @@
+package com.BudgiePanic.rendering.util.pattern;
+
+import com.BudgiePanic.rendering.util.Canvas;
+import com.BudgiePanic.rendering.util.Color;
+
+/**
+ * @author BudgiePanic
+ */
+public final record Texture2D(Canvas texture) implements Pattern2D {
+
+    @Override
+    public Color sample(double u, double v) {
+        v = 1.0 - v;
+        int x = (int) Math.round(u * (texture.getWidth() - 1));
+        int y = (int) Math.round(v * (texture.getHeight() - 1));
+        return texture.getPixel(x, y);
+    }
+    
+}
