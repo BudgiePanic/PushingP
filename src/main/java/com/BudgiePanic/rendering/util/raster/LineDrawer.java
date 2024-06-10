60ee51286888bf07a471911cf3306a2b231ba78f
BudgiePanic <bsanson90@gmail.com>
2024-06-10 22:56:14 +1200

    fix: allow arbitrary x coords in lines

--- src/main/java/com/BudgiePanic/rendering/util/raster/LineDrawer.java
+++ src/main/java/com/BudgiePanic/rendering/util/raster/LineDrawer.java
@@ -13,6 +13,12 @@ public class LineDrawer {
     private LineDrawer() {}
 
     protected static void drawLine(int x0, int y0, int x1, int y1, Canvas drawCanvas, Color lineColor) {
+        if (x0 > x1) {
+            // swap
+            int x = x0, y = y0;
+            x0 = x1; y0 = y1;
+            x1 = x; y1 = y;
+        }
         final double gradient = ((double)(y1 - y0)) / ((double)(x1 - x0));
         double y = y0;
         for (int x = x0; x <= x1; x++) {
