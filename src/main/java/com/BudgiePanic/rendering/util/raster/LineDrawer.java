83256233716833bc26031346013fe4bef64309d3
BudgiePanic <bsanson90@gmail.com>
2024-06-10 22:47:29 +1200

    refactor: simplify line draw even more

--- src/main/java/com/BudgiePanic/rendering/util/raster/LineDrawer.java
+++ src/main/java/com/BudgiePanic/rendering/util/raster/LineDrawer.java
@@ -13,17 +13,11 @@ public class LineDrawer {
     private LineDrawer() {}
 
     protected static void drawLine(int x0, int y0, int x1, int y1, Canvas drawCanvas, Color lineColor) {
-        final double gradient =  ((double)(y1 - y0)) / ((double)(x1 - x0));
-        // the initial point
-        drawCanvas.writePixel(x0, y0, lineColor);
-        // the rest of the points
-        double lastY = y0;
-        for (int x = x0 + 1; x <= x1; x++) {
-            double nextY = lastY + gradient;
-            final int y = (int) Math.round(nextY);
-            drawCanvas.writePixel(x, y, lineColor);
-            lastY = nextY;
+        final double gradient = ((double)(y1 - y0)) / ((double)(x1 - x0));
+        double y = y0;
+        for (int x = x0; x <= x1; x++) {
+            drawCanvas.writePixel(x, (int) Math.round(y), lineColor);
+            y = y + gradient;
         }
     }
-
 }
