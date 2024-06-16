35de5bd3f70d3c99b48574868570559834481ad4
BudgiePanic <bsanson90@gmail.com>
2024-06-16 14:09:09 +1200

    feat: implement line draw steep gradient

--- src/main/java/com/BudgiePanic/rendering/util/raster/LineDrawer.java
+++ src/main/java/com/BudgiePanic/rendering/util/raster/LineDrawer.java
@@ -12,18 +12,51 @@ public class LineDrawer {
     
     private LineDrawer() {}
 
+    /**
+     * Draw a straight line on a canvas
+     * @param x0
+     *   The x coord of the start point of the line
+     * @param y0
+     *   The y coord of the start point of the line
+     * @param x1
+     *   The x coord of the end point of the line
+     * @param y1
+     *   The y coord of the end point of the line
+     * @param drawCanvas
+     *   The canvas the line will be drawn onto
+     * @param lineColor
+     *   The color of the line
+     */
     protected static void drawLine(int x0, int y0, int x1, int y1, Canvas drawCanvas, Color lineColor) {
-        if (x0 > x1) {
-            // swap
-            int x = x0, y = y0;
-            x0 = x1; y0 = y1;
-            x1 = x; y1 = y;
-        }
-        final double gradient = ((double)(y1 - y0)) / ((double)(x1 - x0));
-        double y = y0;
-        for (int x = x0; x <= x1; x++) {
-            drawCanvas.writePixel(x, (int) Math.round(y), lineColor);
-            y = y + gradient;
+        final int xStep = x1 - x0, yStep = y1 - y0;
+        if (Math.abs(xStep) > Math.abs(yStep)) {
+            // draw a horizontal line
+            if (x0 > x1) {
+                // swap
+                int x = x0, y = y0;
+                x0 = x1; y0 = y1;
+                x1 = x; y1 = y;
+            }
+            final double gradient = ((double)yStep) / ((double)xStep);
+            double y = y0;
+            for (int x = x0; x <= x1; x++) {
+                drawCanvas.writePixel(x, (int) Math.round(y), lineColor);
+                y = y + gradient;
+            }
+        } else {
+            // draw a vertical line
+            if (y0 > y1) {
+                // swap
+                int x = x0, y = y0;
+                x0 = x1; y0 = y1;
+                x1 = x; y1 = y;
+            }
+            final double gradient = ((double)xStep) / ((double)yStep);
+            double x = x0;
+            for (int y = y0; y <= y1; y++) {
+                drawCanvas.writePixel((int) Math.round(x), y, lineColor);
+                x = x + gradient;
+            }
         }
     }
 }
