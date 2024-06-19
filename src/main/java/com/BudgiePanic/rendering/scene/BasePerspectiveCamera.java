2c0495e16422001268da50c0b94e4810f2b94369
BudgiePanic <bsanson90@gmail.com>
2024-06-19 14:19:45 +1200

    feat: implement camera view frustum culling

--- src/main/java/com/BudgiePanic/rendering/scene/BasePerspectiveCamera.java
+++ src/main/java/com/BudgiePanic/rendering/scene/BasePerspectiveCamera.java
@@ -1,11 +1,12 @@
 package com.BudgiePanic.rendering.scene;
 
 import static com.BudgiePanic.rendering.util.Tuple.makePoint;
-import static com.BudgiePanic.rendering.util.Tuple.makeVector;
 
+import java.util.Arrays;
 import java.util.Optional;
 
 import com.BudgiePanic.rendering.util.Directions;
+import com.BudgiePanic.rendering.util.FloatHelp;
 import com.BudgiePanic.rendering.util.Pair;
 import com.BudgiePanic.rendering.util.Tuple;
 import com.BudgiePanic.rendering.util.matrix.Matrix4;
@@ -50,11 +51,31 @@ public abstract class BasePerspectiveCamera implements Camera {
      */
     protected final double focalDistance;
 
+    /**
+     * The near clipping plane. Located 'focal distance' units away from the camera origin (0,0,0) in the -ve z direction.
+     * Lies flat against the XY plane.
+     */
     final ClippingPlane near;
+    /**
+     * The left clipping plane.
+     */
     final ClippingPlane left;
+    /**
+     * The right clipping plane.
+     */
     final ClippingPlane right;
+    /**
+     * The top clipping plane.
+     */
     final ClippingPlane top;
+    /**
+     * The bottom clipping plane.
+     */
     final ClippingPlane bottom;
+    /**
+     * All planes collected together to allow iteration over the planes.
+     */
+    final ClippingPlane[] planes;
 
     /**
      * Create a new Base perspective camera.
@@ -87,6 +108,7 @@ public abstract class BasePerspectiveCamera implements Camera {
         this.right = new ClippingPlane(zero, Rotation.buildYRotationMatrix(-fov * 0.5).multiply(forward).normalize());
         this.top = new ClippingPlane(zero, Rotation.buildXRotationMatrix(-fov * 0.5).multiply(forward).normalize());
         this.bottom = new ClippingPlane(zero, Rotation.buildXRotationMatrix(fov * 0.5).multiply(forward).normalize());
+        planes = new ClippingPlane[] {near, left, right, top, bottom};
     }
 
     @Override
@@ -121,6 +143,13 @@ public abstract class BasePerspectiveCamera implements Camera {
     }
 
     protected final record ClippingPlane(Tuple point, Tuple normal) {
+        /**
+         * Calculate the signed straightline distance between a point and this clipping plane.
+         * @param point
+         *   A point in local camera space.
+         * @return
+         *   The signed distance between the point and the plane.
+         */
         double distanceTo(Tuple point) {
             final double a = normal.x, b = normal.y, c = normal.z;
             final double x0 = this.point.x, y0 = this.point.y, z0 = this.point.z;
@@ -130,11 +159,69 @@ public abstract class BasePerspectiveCamera implements Camera {
             final double denominator = Math.sqrt(a*a + b*b + c*c);
             return numerator / denominator;
         }
-        boolean pointInside(Tuple localPoint) { 
-            return distanceTo(localPoint) > 0.0; 
+        /**
+         * Determine whether a point is considered 'inside' a plane.
+         * @param localPoint
+         *   A point in local camera space.
+         * @return
+         *   True of the point is 'inside' of the clipping plane.
+         */
+        boolean pointInside(Tuple localPoint) { return distanceTo(localPoint) > 0.0; }
+        /**
+         * Clip a line segment against the clipping plane. 
+         * This method should only be called when one point is inside the plane and the other is outside the plane.
+         * Doing so otherwise is a waste of effort 
+         * (both points inside = line segment is completely within the camera's FOV, both points outside = camera can't see the line segment)
+         *
+         * @param inside
+         *   A point that is inside the clipping plane.
+         * @param outside
+         *   A point that is outside the clipping plane.
+         * @return
+         *   A new point that is the intersection point between the line segment and the clipping plane.
+         */
+        Tuple clip(Tuple inside, Tuple outside) { 
+            Tuple direction = outside.subtract(inside);
+            double denominator = normal.dot(direction);
+            if (FloatHelp.compareFloat(0.0, denominator) == 0) {
+                throw new RuntimeException("Could not intersect line " + Arrays.toString(new Tuple[] {inside, outside}) + " with plane " + this.toString());
+            }
+            double numerator = normal.dot(point.subtract(inside));
+            double distance = numerator / denominator;
+            Tuple intersection = inside.add(direction.multiply(distance));
+            return intersection; 
         }
-        Pair<Tuple, Tuple> clip(Tuple a, Tuple b) { return null; }
     }
 
+    /**
+     * Clips the points defining the start and end of a line segment against the camera's view frustrum.
+     *
+     * @param pointA
+     *   The start of the line segment. A point in local camera space.
+     * @param pointB
+     *   The end of the line segment. A point in local camera space.
+     * @return
+     *   The start and end of the line segment that can be seen by the camera. 
+     *   An empty optional is returned if no part of the line segment can be seen by the camera.
+     */
+    public final Optional<Pair<Tuple, Tuple>> clip(Tuple pointA, Tuple pointB) {
+        for (final ClippingPlane plane : planes) {
+            double distanceToPlaneA = plane.distanceTo(pointA);
+            boolean isAInside = distanceToPlaneA > 0;
+            double distanceToPlaneB = plane.distanceTo(pointB);
+            boolean isBInside = distanceToPlaneB > 0;
+            if (!isAInside && !isBInside) { return Optional.empty(); }
+            if (isAInside && isBInside) { continue; }
+            if (isAInside) {
+                Tuple intersection = plane.clip(pointA, pointB);
+                pointB = intersection;
+            } else {
+                assert isBInside;
+                Tuple intersection = plane.clip(pointB, pointA);
+                pointA = intersection;
+            }
+        }
+        return Optional.of(new Pair<>(pointA, pointB));
+    }
 }
 
