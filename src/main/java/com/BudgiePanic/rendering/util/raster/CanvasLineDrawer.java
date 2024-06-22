21d0eb9bf11416ff2df6d01f8d9f480b1cb259ea
BudgiePanic <bsanson90@gmail.com>
2024-06-22 17:13:09 +1200

    feat: add canvas line drawer depth buffer overload
    canvas line drawer method that considers the depth of each point on
    the line segment before it writes a pixel to the canvas.

--- src/main/java/com/BudgiePanic/rendering/util/raster/CanvasLineDrawer.java
+++ src/main/java/com/BudgiePanic/rendering/util/raster/CanvasLineDrawer.java
@@ -41,7 +41,7 @@ public final class CanvasLineDrawer {
                 x0 = x1; y0 = y1;
                 x1 = x; y1 = y;
             }
-            int[] yValues = linearInterpolate(x0, y0, x1, y1, new int[1 + (x1-x0)]);
+            final int[] yValues = linearInterpolate(x0, y0, x1, y1, new int[1 + (x1-x0)]);
             for (int x = x0; x <= x1; x++) {
                 drawCanvas.writePixel(x, yValues[x - x0], lineColor);
             }
@@ -53,9 +53,56 @@ public final class CanvasLineDrawer {
                 x0 = x1; y0 = y1;
                 x1 = x; y1 = y;
             }
-            int[] xValues = linearInterpolate(y0, x0, y1, x1, new int[1 + (y1-y0)]);
+            final int[] xValues = linearInterpolate(y0, x0, y1, x1, new int[1 + (y1-y0)]);
             for (int y = y0; y <= y1; y++) {
                 drawCanvas.writePixel(xValues[y - y0], y, lineColor);
+            }
+        }
+    }
+
+    public final static synchronized void drawLineDepth(int x0, int y0, double z0, int x1, int y1, double z1, Canvas drawCanvas, Canvas depthBuffer, Color lineColor) {
+        int[] clamped = boundsCheck(x0, y0, x1, y1, drawCanvas);
+        x0 = clamped[0]; y0 = clamped[1]; x1 = clamped[2]; y1 = clamped[3];
+        final int xStep = x1 - x0, yStep = y1 - y0;
+        if (Math.abs(xStep) > Math.abs(yStep)) {
+            // draw a horizontal line
+            if (x0 > x1) {
+                // swap
+                int x = x0, y = y0;
+                x0 = x1; y0 = y1;
+                x1 = x; y1 = y;
+                double z = z0;
+                z0 = z1; z1 = z;
+            }
+            final int[] yValues = linearInterpolate(x0, y0, x1, y1, new int[1 + (x1-x0)]);
+            final double[] zInvValues = linearInterpolate(x0, 1.0 / z0, x1, 1.0 / z1, new double[1 + (x1-x0)]);
+            for (int x = x0; x <= x1; x++) {
+                final int y = yValues[x - x0];
+                final double depth = 1.0 / zInvValues[x - x0];
+                if (depth < depthBuffer.getPixel(x, y).x) { 
+                    drawCanvas.writePixel(x, y, lineColor);
+                    depthBuffer.writePixel(x, y, new Color(depth, depth, depth));
+                }
+            }
+        } else {
+            // draw a vertical line
+            if (y0 > y1) {
+                // swap
+                int x = x0, y = y0;
+                x0 = x1; y0 = y1;
+                x1 = x; y1 = y;
+                double z = z0;
+                z0 = z1; z1 = z;
+            }
+            final int[] xValues = linearInterpolate(y0, x0, y1, x1, new int[1 + (y1-y0)]);
+            final double[] zInvValues = linearInterpolate(y0, 1.0 / z0, y1, 1.0 / z1, new double[1 + (y1-y0)]);
+            for (int y = y0; y <= y1; y++) {
+                final int x = xValues[y-y0];
+                final double depth = 1.0 / zInvValues[y - y0];
+                if (depth < depthBuffer.getPixel(x, y).x) {
+                    drawCanvas.writePixel(xValues[y - y0], y, lineColor);
+                    depthBuffer.writePixel(x, y, new Color(depth, depth, depth));
+                }
             }
         }
     }
@@ -110,6 +157,33 @@ public final class CanvasLineDrawer {
         double dependent = d0;
         for (int i = i0; i <= i1; i++) {
             result[i - i0] = (int) Math.round(dependent);
+            dependent += gradient;
+        }
+        return result;
+    }
+
+    /**
+     * Determine the interpolated values for a linear function ranging from i0 to i1, that outputs values ranging from d0 to d1.
+     *
+     * @param i0
+     *   The first input into the linear function
+     * @param d0
+     *   The first output of the linear function
+     * @param i1
+     *   The last input into the linear function
+     * @param d1
+     *   The last output of the linear function
+     * @param result
+     *   An array to write the interpolated function output values into. The array length should match the length of the pixel array
+     * @return
+     *   Returns the inputted result array.
+     */
+    protected static final double[] linearInterpolate(double i0, double d0, double i1, double d1, double[] result) {
+        assert result != null;
+        final double gradient = (d1 - d0)/(i1 - i0);
+        double dependent = d0;
+        for (int i = 0; i < result.length; i++) {
+            result[i] = dependent;
             dependent += gradient;
         }
         return result;
